5646947d0163373df7fe289ab840bbfe4c9c559e
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-12 20:17:36 +0800

    menambahkan setFileUrl sebagai argument

--- src/hooks/incomingmail/useIncomingmailEffect.jsx
+++ src/hooks/incomingmail/useIncomingmailEffect.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react"
 import useIncomingmailActions from "./useIncomingmailActions"
 
-const useIncomingmailEffect = (url, token, command, setData, setIData, setCommand)=>{
+const useIncomingmailEffect = (url, token, command, setData, setIData, setFileUrl, setCommand)=>{
      useEffect(()=>{
-          useIncomingmailActions(url, token, command, setData, setIData, setCommand)
+          useIncomingmailActions(url, token, command, setData, setIData, setFileUrl, setCommand)
      }, [command])
 }
 

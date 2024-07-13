dc96ce9516788124a5ce7e1e2722880bf40197ca
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-13 16:25:43 +0800

    mengganti dengan FormData

--- src/hooks/useFormData.jsx
+++ src/hooks/useFormData.jsx
@@ -1,15 +1,15 @@
 const useFormData = (id)=>{
      const form = document.getElementById(id)
-     const data = {
-          'agenda_number' : form.querySelector('#agenda_number').value,
-          'letter_number' : form.querySelector('#letter_number').value,
-          'source' : form.querySelector('#source').value,
-          'letter_date' : form.querySelector('#letter_date').value,
-          'received_date' : form.querySelector('#received_date').value,
-          'recipient' : form.querySelector('#recipient').value,
-          'subject' : form.querySelector('#subject').value,
-     }
-     return data
+     const formData = new FormData()
+     formData.append('agenda_number', form.querySelector('#agenda_number').value)
+     formData.append('letter_number', form.querySelector('#letter_number').value)
+     formData.append('source', form.querySelector('#source').value)
+     formData.append('letter_date', form.querySelector('#letter_date').value)
+     formData.append('received_date', form.querySelector('#received_date').value)
+     formData.append('recipient', form.querySelector('#recipient').value)
+     formData.append('subject', form.querySelector('#subject').value)
+     formData.append('file', form.querySelector('#document').files[0])
+     return formData
 }
 
 export default useFormData

67d99f81ab1e7cb76aedce613fd32353a72a9ea6
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-16 18:30:24 +0800

    menambahkan alert success dan error

--- src/hooks/incomingmail/useHandlePost.jsx
+++ src/hooks/incomingmail/useHandlePost.jsx
@@ -1,9 +1,15 @@
 import usePostData from '../request/usePostData'
+import useAlert from '../useAlert'
 
 const useHandlePost = (url, token, command, setCommand)=>{
-     usePostData(url, command.data, token, (response)=>{
+     usePostData(url, command.data, token, 
+     (response)=>{
           console.log(response)
           setCommand(null)
+          useAlert('success')
+     }, 
+     (err)=>{
+          useAlert('error')
      })
 }
 

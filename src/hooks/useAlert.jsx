5326389baef084584e3198c8e2a7b54f27331eb6
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-16 18:59:43 +0800

    menambahkan success delete dan error delete

--- src/hooks/useAlert.jsx
+++ src/hooks/useAlert.jsx
@@ -8,12 +8,28 @@ const useAlert = (option)=>{
                     text: "Data telah berhasil ditambahkan!",
                     icon: "success"
                   })
+               break
+          case 'success_delete':
+               Swal.fire({
+                    title: "Berhasil dihapus!",
+                    text: "Data berhasil dihapus!",
+                    icon: "success"
+                  })
+               break
+          case 'error_delete':
+                    Swal.fire({
+                         title: "Oppss gagal menghapus data!!!",
+                         text: "Mungkin terjadi kesalahan di server!!!",
+                         icon: "error"
+                    })
+               break
           case 'error':
                Swal.fire({
                     title: "Oppss terjadi kesalahan!!!",
                     text: "Coba periksa input yang ada berikan!!!",
                     icon: "error"
                })
+               break
      }
 }
 

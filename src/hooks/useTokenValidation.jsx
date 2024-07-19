c2f4fbc74533d685cc2a23d4d6b9b0792e02002b
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-19 16:59:12 +0800

    first commit

---
+++ src/hooks/useTokenValidation.jsx
@@ -0,0 +1,15 @@
+import { jwtDecode } from "jwt-decode"
+
+const useTokenValidation = (accessToken)=>{
+     if (accessToken.length > 0){
+          const jwt = jwtDecode(accessToken)
+          const date = new Date()
+          
+          if (!(date.getTime() <= jwt.exp*1000)){
+               return false
+          }
+     }
+     return accessToken
+}
+
+export default useTokenValidation

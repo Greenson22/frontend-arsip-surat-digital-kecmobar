420bb382c163df79f5256837ee22ef53a3eed866
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-19 17:00:53 +0800

    menambah penanganan login

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 import { LoginLayout } from '../Layouts'
+import { useNavigate } from 'react-router-dom'
+
+import axios from 'axios';
+import { useAlert } from '../../hooks'
 
 
 function LoginPage() {
+     const navigate = useNavigate()
+
      const handleSubmit = (event)=>{
-          console.log(event.target.name)
+          event.preventDefault()
+          const formData = new FormData(event.target)
+          formData.append('username', event.target.username.value)
+          formData.append('password', event.target.password.value)
+
+          useAlert('loading')
+
+          axios.post('http://localhost:8000/api/token/', formData)
+          .then((response)=>{
+               const data = response.data
+               localStorage.setItem('accessToken', data.access)
+               localStorage.setItem('refreshToken', data.refresh)
+               useAlert('success_login')
+               navigate('/incoming_mail')
+          })
+          .catch((error)=>{
+               console.log(error)
+               useAlert('error_login')
+          })
      }
 
      return (
@@ -15,13 +39,15 @@ function LoginPage() {
                          <span className="h2">Arsip surat digital</span>
                     </div>
 
-                    <MDBInput label="Pengguna" type="text" className='mb-2' size="lg"/>
-                    <MDBInput label="Password" type="password" className='mb-2' size="lg"/>
+                    <MDBInput id='username' label="Pengguna" type="text" className='mb-2' size="lg" required/>
+                    <MDBInput id='password' label="Password" type="password" className='mb-2' size="lg" required/>
+                    
                     <div className="mb-4 d-grid">
                          <MDBBtn color='dark' size='lg'>Login</MDBBtn>
                     </div>
                </form>
           </LoginLayout>
-     )}
+          )
+     }
 
 export default LoginPage

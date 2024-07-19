76e1c8610cbdbed7cd1dfe282cd16a349c499963
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-19 11:08:34 +0800

    menambah catch error

--- src/hooks/incomingmail/useHandleFetch.jsx
+++ src/hooks/incomingmail/useHandleFetch.jsx
@@ -1,8 +1,15 @@
+import axios from 'axios'
 import useFetchData from '../request/useFetchData'
 
 const useHandleFetch = (newUrl, token, setData)=>{
      useFetchData(newUrl, token, (response)=>{
           setData(response['data'])
+     }, 
+     (error)=>{
+          if (error.response && error.response.status == 401){
+               console.log(error.response)
+               axios.post()
+          }
      })
 }
 

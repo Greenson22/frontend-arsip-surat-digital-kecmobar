f75c97054d368dcb7391bee6c851b519b236f1e8
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-21 11:05:32 +0800

    menambahkan IncomingMailLayout dan IncomingMailEmptyLayout

--- src/components/Layouts/index.jsx
+++ src/components/Layouts/index.jsx
@@ -1,2 +1,4 @@
 export { default as ContainerLayout } from './ContainerLayout'
 export { default as LoginLayout } from './LoginLayout'
+export { default as IncomingMailLayout } from './IncomingMailLayout'
+export { default as IncomingMailEmptyLayout } from './IncomingMailEmptyLayout'

0e3ed912039ce2152c14250c118efbd51362fac1
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-21 11:03:05 +0800

    menambahkan fitur disabled pada button 2 atau export

--- src/components/Fragments/TableAction.jsx
+++ src/components/Fragments/TableAction.jsx
@@ -1,15 +1,21 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { MDBBtn } from "mdb-react-ui-kit"
 
 const TableAction = (props)=>{
-     const {title, button1, button1_target, button2, button2_target} = props
+     const {title, button1, button1_target, button2, button2_target, button2Disabled=false} = props
+     const btnExportRef = useRef()
+
+     useEffect(()=>{
+          btnExportRef.current.disabled = button2Disabled
+     }, [])
+
      return(
           <div className="row">
                <div className="col-12">
                     <h5 className="card-title mt-1 float-start">{title}</h5>
                     <div className="btn-group float-end">
                          <MDBBtn outline data-bs-toggle="modal" data-bs-target={button1_target}>{button1}</MDBBtn>
-                         <MDBBtn color="success" outline  data-bs-toggle="modal" data-bs-target={button2_target}>{button2}</MDBBtn>
+                         <MDBBtn color="success" outline  data-bs-toggle="modal" ref={btnExportRef} data-bs-target={button2_target}>{button2}</MDBBtn>
                     </div>
                </div>
           </div>

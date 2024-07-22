53980da460120cba6609ae1e6c5950c931b2c65f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-22 21:25:56 +0800

    menambahkan useEffect dan dataRef

--- src/components/Layouts/ContainerLayout.jsx
+++ src/components/Layouts/ContainerLayout.jsx
@@ -1,14 +1,19 @@
 import Sidebar from '../Fragments/Sidebar'
 import Navbar from '../Fragments/Navbar'
+import { useRef } from 'react'
+import { useContainerLayoutEffect } from '../../hooks'
 
 const ContainerLayout = (props) => {
   const {children} = props
+  const dataRef = useRef()
+  
+  useContainerLayoutEffect(dataRef)
 
   return(
     <div className='wrapper'>
       <Sidebar/>
       <div id="content" className='active'>
-        <Navbar date="27 Mei 2024" user="Admin"/>
+        <Navbar date="27 Mei 2024" user="Admin" ref={dataRef}/>
         <div className="container-fluid">
           {children} {/* Konten */}
         </div>

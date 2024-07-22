2f06a0396a4e8bb75827d9121f43f50682787cdd
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-22 19:56:35 +0800

    menghapus useSelector, url, state fileUlr

--- src/components/pages/incoming_mail.jsx
+++ src/components/pages/incoming_mail.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from "react"
-import { useSelector } from "react-redux"
 import { useIncomingmailEffect } from "../../hooks"
 import useLoginValidate from "../../hooks/useLoginValidation"
-
 import { IncomingMailEmptyLayout, IncomingMailLayout } from '../Layouts'
 
 function IncomingMailPage() {
-     const url = useSelector((state) => state.api.incomingmail)
-
-     const accessToken = localStorage.getItem('accessToken')
-     useLoginValidate(accessToken)
-
      const [data, setData] = useState(null)
      const [iData, setIData] = useState(0)
      const [command, setCommand] = useState(null)
-     const [fileUrl, setFileUrl] = useState(null)
+     const accessToken = localStorage.getItem('accessToken')
      
-     useIncomingmailEffect(url, accessToken, command, setData, setIData, setFileUrl, setCommand)
-
+     useLoginValidate(accessToken)
+     useIncomingmailEffect(command, setData, setIData, setCommand)
+     
      return (data && data['count'] > 0) ? 
-     (<IncomingMailLayout url={url} data={data} iData={iData} command={command} setCommand={setCommand}/>) :
+     (<IncomingMailLayout data={data} iData={iData} command={command} setCommand={setCommand}/>) :
      (<IncomingMailEmptyLayout setCommand={setCommand}/>)
 
 }

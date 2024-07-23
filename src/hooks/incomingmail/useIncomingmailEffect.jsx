0ff1c3a93c8e2bf05934a78c950b5780e360931b
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-23 08:23:22 +0800

    menghapus search_value di local storage saat halaman pertama kali dirender

--- src/hooks/incomingmail/useIncomingmailEffect.jsx
+++ src/hooks/incomingmail/useIncomingmailEffect.jsx
@@ -5,6 +5,10 @@ const useIncomingmailEffect = (command, setData, setIData, setCommand)=>{
      useEffect(()=>{
           useIncomingmailActions(command, setData, setIData, setCommand)
      }, [command])
+     useEffect(()=>{
+          console.log("incoming mail dibuat")
+          localStorage.setItem('search_value', '')
+     }, [])
 }
 
 export default useIncomingmailEffect

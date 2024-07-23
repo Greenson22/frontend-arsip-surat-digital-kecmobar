30b8aef959d0e6a4df5b61553dcd47cc1a0f3fcd
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-23 08:18:25 +0800

    menghapus import axios

--- src/components/pages/incoming_mail.jsx
+++ src/components/pages/incoming_mail.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import { useAlert, useIncomingmailEffect } from "../../hooks"
 import useLoginValidate from "../../hooks/useLoginValidation"
 import { IncomingMailEmptyLayout, IncomingMailLayout } from '../Layouts'
-import axios from "axios"
 
 function IncomingMailPage() {
      const [data, setData] = useState(null)

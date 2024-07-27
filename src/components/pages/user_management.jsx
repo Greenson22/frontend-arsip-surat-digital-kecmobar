684cd418bdcb7991133a3e4ec5f7b481c4d77074
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-27 20:17:43 +0800

    melakukan factoring

--- src/components/pages/user_management.jsx
+++ src/components/pages/user_management.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from "react"
-import {Card, CardHeader, CardBody} from "../Elements/Card"
-import {Table, TableHead, TableBody, RowAction} from "../Elements/Table"
-import { TitleBar, TableFilter, TableAction, AddUserModal, ExportModal, EditUserModal } from '../Fragments'
-
-import ViewProfileModal from "../Fragments/Modals/ViewProfileModal"
+import { useState } from "react"
+import { UserManagementLayout } from "../Layouts"
 import useLoginValidate from "../../hooks/useLoginValidation"
 
-import pengguna from "../../assets/data/pengguna.json"
-
 const UserManagementPage = ()=>{
-     const columns = ["No", "Nama", "Pengguna", "Tingkat", "Status", "Registrasi", "Tindakan"]
 
      const accessToken = localStorage.getItem('accessToken')
      useLoginValidate(accessToken)
@@ -17,41 +10,7 @@ const UserManagementPage = ()=>{
      const [data, setData] = useState([])
 
      return(
-          <div>
-               <TitleBar>Pengaturan pengguna</TitleBar>
-               <Card>
-                    <CardHeader>
-                         <TableAction title="Daftar pengguna" button1="Tambah Pengguna" button1_target="#addUserModal" button2="Ekspor" button2_target="#exportModal"/>
-                    </CardHeader>
-                    <CardBody>
-                         <TableFilter/>
-                         <Table add_class="table-sm">
-                              <TableHead columns = {columns}/>
-                              <TableBody>
-                                   {data.map((user, index)=>{
-                                             return(
-                                                  <tr key={user.id}>
-                                                       <td>{index+1}</td>
-                                                       <td>{user.first_name+' '+user.last_name}</td>
-                                                       <td>{user.username}</td>
-                                                       <td>{user.is_superuser ? 'admin' : 'user'}</td>
-                                                       <td>{user.is_active ? 'aktif' : 'nonaktif'}</td>
-                                                       <td>{user.date_joined}</td>
-                                                       <td><RowAction ubah_target='#editUserModal' lihat_target='#viewProfileModal'/></td>
-                                                  </tr>
-                                             )
-                                        })}
-                              </TableBody>
-                         </Table>
-                    </CardBody>
-               </Card>
-
-               {/* Modal */}
-               <AddUserModal/>
-               <EditUserModal/>
-               <ViewProfileModal pengguna={pengguna} i_pengguna='0'/>
-               <ExportModal title="Ekspor daftar pengguna"/>
-          </div>
+          <UserManagementLayout data={data}/>
      )
 }
 

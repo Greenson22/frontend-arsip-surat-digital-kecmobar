7f267de3c1e46c26caca969a1ea568552a18976f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-27 21:57:56 +0800

    menambahkan data state, iData state, comman state dan usePageEffect

--- src/components/pages/user_management.jsx
+++ src/components/pages/user_management.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react"
 import { UserManagementLayout } from "../Layouts"
-import useLoginValidate from "../../hooks/useLoginValidation"
+import { useLoginValidate, usePageEffect, useUserManagementActions } from "../../hooks"
 
 const UserManagementPage = ()=>{
-
-     const accessToken = localStorage.getItem('accessToken')
-     useLoginValidate(accessToken)
-
      const [data, setData] = useState([])
-
+     const [iData, setIData] = useState([])
+     const [command, setCommand] = useState([])
+     const accessToken = localStorage.getItem('accessToken')
+     
+     useLoginValidate(accessToken)
+     usePageEffect(command, setData, setIData, setCommand, useUserManagementActions)
+     
      return(
           <UserManagementLayout data={data}/>
      )

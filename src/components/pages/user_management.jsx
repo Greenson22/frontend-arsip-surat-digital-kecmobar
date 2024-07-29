4cba928df7b71dbf9c86d823824c6a755e49d69f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-29 09:18:46 +0800

    menambah UserManagementEmptyLayout

--- src/components/pages/user_management.jsx
+++ src/components/pages/user_management.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react"
-import { UserManagementLayout } from "../Layouts"
+import { UserManagementLayout, UserManagementEmptyLayout } from "../Layouts"
 import { useLoginValidate, usePageEffect, useUserManagementActions } from "../../hooks"
 
 const UserManagementPage = ()=>{
      const [data, setData] = useState([])
      const [iData, setIData] = useState([])
-     const [command, setCommand] = useState([])
+     const [command, setCommand] = useState(null)
      const accessToken = localStorage.getItem('accessToken')
      
      useLoginValidate(accessToken)
      usePageEffect(command, setData, setIData, setCommand, useUserManagementActions)
-     
-     return(
-          <UserManagementLayout data={data}/>
-     )
+
+     return (data && data['count'] > 0) ? 
+     ( <UserManagementLayout data={data} setCommand={setCommand}/> ):
+     ( <UserManagementEmptyLayout setCommand={setCommand}/> )
 }
 
 export default UserManagementPage

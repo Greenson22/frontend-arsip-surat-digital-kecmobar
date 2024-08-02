2e1f7704dfb24511aa544de2a232d6a5983c9031
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-02 19:08:46 +0800

    memindahkan ke folder effects

--- src/hooks/effects/useContainerLayoutEffect.jsx
+++ src/hooks/effects/useContainerLayoutEffect.jsx
@@ -1,13 +1,17 @@
 import axios from "axios"
+import { jwtDecode } from "jwt-decode"
 import { useEffect } from "react"
 
-const useContainerLayoutEffect = (dataRef)=>{
-     useEffect(()=>{
+const useContainerLayoutEffect = (navRef)=>{
+  useEffect(()=>{
           function handleAxiosResponse(response){
             const namaBulan = [ "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" ]
             const unixTime = response.data.unixtime
             const dateTime = new Date(unixTime*1000)
-            dataRef.current.textContent = dateTime.getDate()+' '+namaBulan[dateTime.getMonth()]+' '+dateTime.getFullYear()
+            
+            const jwt = jwtDecode(localStorage.getItem('accessToken'))
+            navRef.current.querySelector("#date").textContent = dateTime.getDate()+' '+namaBulan[dateTime.getMonth()]+' '+dateTime.getFullYear()
+            navRef.current.querySelector('#user-name').textContent = jwt.first_name+' '+jwt.last_name
           }
           function handleAxiosError(err){
             console.log(err)

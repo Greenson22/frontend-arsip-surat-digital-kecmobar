52786840221527990f776dea30042ac1b8c8a41c
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-07 19:38:20 +0800

    menambahkan api di komponen TableFilter

--- src/components/Layouts/UserManagementLayout.jsx
+++ src/components/Layouts/UserManagementLayout.jsx
@@ -1,12 +1,15 @@
 import React from "react"
+import { MDBBtn } from "mdb-react-ui-kit"
 import { Card, CardHeader, CardBody } from "../Elements/Card"
 import { Table, TableHead } from "../Elements/Table"
 import { TitleBar, TableFilter, TableAction, UserManagementTableBody,
      AddUserModal, ExportModal, EditUserModal } from '../Fragments'
 
 import ViewProfileModal from "../Fragments/Modals/ViewProfileModal"
+import { useSetPage } from "../../hooks"
 
 const UserManagementLayout = (props)=>{
+     const api = import.meta.env.VITE_USERS_API_KEY
      const {data, iData, command, setCommand} = props
      const columns = ["No", "Nama", "Pengguna", "Tingkat", "Status", "Registrasi", "Tindakan"]
 
@@ -19,11 +22,13 @@ const UserManagementLayout = (props)=>{
                          <TableAction title="Daftar pengguna" button1="Tambah Pengguna" button1_target="#addUserModal" button2="Ekspor" button2_target="#exportModal"/>
                     </CardHeader>
                     <CardBody>
-                         <TableFilter setCommand={setCommand}/>
+                         <TableFilter setCommand={setCommand} api={api}/>
                          <Table add_class="table-sm">
                               <TableHead columns = {columns}/>
                               <UserManagementTableBody data={data['results']} setCommand={setCommand}/>
                          </Table>
+                         <MDBBtn onClick={() => { useSetPage(data['previous'], setCommand) } }>prev</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['next'], setCommand) } }>next</MDBBtn>
                     </CardBody>
                </Card>
 

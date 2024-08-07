967b900147b78ea5293c955198a9650a24e515c3
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-07 17:38:46 +0800

    membuat fungsi yang menangani perubahan password

--- src/components/Fragments/Modals/EditPasswordModal.jsx
+++ src/components/Fragments/Modals/EditPasswordModal.jsx
@@ -2,21 +2,56 @@ import React from "react"
 
 import {Modal, ModalHeader, ModalBody, ModalFooter} from "../../Elements/Modal"
 import { MDBInput, MDBBtn } from "mdb-react-ui-kit"
+import { useAlert } from "../../../hooks"
 
-const EditPasswordModal = ()=>{
+const EditPasswordModal = (props)=>{
+     const {user, setCommand} = props
+
+     const onHandleSubmit = (event)=>{
+          event.preventDefault()
+          
+          const oldPassword = event.target['old-password'].value
+          const newPassword = event.target['new-password'].value
+          const passwordConfirm = event.target['new-password-confirm'].value
+          
+          const formData = new FormData()
+          formData.append('username', user.username)
+          formData.append('is_active', user.is_active);
+          formData.append('is_superuser', user.is_superuser);
+          
+          // --- Password Validation Logic ---
+          if (oldPassword) { // Check if old password field is not empty
+               if (newPassword !== passwordConfirm) { // Check if new password and confirm password match
+                    useAlert('error_password_not_match')
+                    return; // Stop submission if passwords don't match
+                    }
+                    formData.append('old_password', oldPassword);
+                    formData.append('password', newPassword);
+               }
+          else if (newPassword || passwordConfirm) { 
+               useAlert('error_old_password_not_fill')
+               return; // Stop submission if old password is not provided but new passwords are
+          }
+          setCommand({
+               'command': 'put',
+               'data': formData,
+               'id': user.id
+          });
+     }
+
      return(
           <Modal id="editPasswordModal">
                <ModalHeader title="Ubah Password"/>
                <ModalBody>
-                    <form action="">
-                         <MDBInput label="Password lama" type="text" className='mb-2'/>
-                         <MDBInput label="Password baru" type="text" className='mb-2'/>
-                         <MDBInput label="Konfirmasi password baru" type="text" className='mb-2'/>
+                    <form action="" onSubmit={onHandleSubmit} id="form-edit-password">
+                         <MDBInput label="Password lama" id="old-password" type="text" className='mb-2' required/>
+                         <MDBInput label="Password baru" id="new-password" type="text" className='mb-2' required/>
+                         <MDBInput label="Konfirmasi password baru" id="new-password-confirm" type="text" className='mb-2' required/>
                     </form>
                </ModalBody>
                <ModalFooter>
                     <MDBBtn color="secondary" size="sm" data-bs-dismiss="modal">Tutup</MDBBtn>
-                    <MDBBtn color="warning" size="sm">Simpan Perubahan</MDBBtn>
+                    <MDBBtn color="warning" size="sm" form="form-edit-password">Simpan Perubahan</MDBBtn>
                </ModalFooter>
           </Modal>
      )

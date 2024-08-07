1cbc11904bd37c8f487f58a71120394ab969c3f0
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-07 17:40:33 +0800

    menambahkan fungsi callback yang menangani error

--- src/hooks/request/useFetchFileProfilePicture.jsx
+++ src/hooks/request/useFetchFileProfilePicture.jsx
@@ -1,10 +1,10 @@
 import { useFetchFile } from ".."
 
-const useFetchFileProfilePicture = (id, callbackResponse)=>{
+const useFetchFileProfilePicture = (id, callbackResponse, callbackError)=>{
      const accessToken = localStorage.getItem('accessToken')
      const api = import.meta.env.VITE_USERS_API_KEY.split('?')[0]
     
-     useFetchFile(api+id+'/profile_picture/', accessToken, callbackResponse)
+     useFetchFile(api+id+'/profile_picture/', accessToken, callbackResponse, callbackError)
 }
 
 export default useFetchFileProfilePicture

ee46ca1a07d3061d5179fda59a4efed7dfd75806
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-07 19:26:44 +0800

    mengubah const incomingmailPagination menjadi pagination

--- src/components/Fragments/IncomingMailTableBody.jsx
+++ src/components/Fragments/IncomingMailTableBody.jsx
@@ -3,14 +3,14 @@ import {TableBody, RowAction} from '../Elements/Table'
 
 const IncomingMailTableBody = (props)=>{
      const {data, setCommand} = props
-     const incomingmailPagination = JSON.parse(localStorage.getItem('pagination'))
+     const pagination = JSON.parse(localStorage.getItem('pagination'))
 
      return(
           <TableBody>
                {data.map((surat, index)=>{
                          return(
                               <tr key={surat.id}>
-                                   <td>{index+1+((incomingmailPagination['page']-1)*incomingmailPagination['page_size'])}</td>
+                                   <td>{index+1+((pagination['page']-1)*pagination['page_size'])}</td>
                                    <td >{surat.agenda_number}</td>
                                    <td >{surat.letter_number}</td>
                                    <td >{surat.letter_date}</td>

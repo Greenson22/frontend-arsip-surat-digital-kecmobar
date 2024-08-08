63f08184f70b2aa231189652049deb8df09b498a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-08 20:24:50 +0800

    menambahkan optional chaining

--- src/hooks/effects/useEditIncomingMailModalEffect.jsx
+++ src/hooks/effects/useEditIncomingMailModalEffect.jsx
@@ -3,13 +3,13 @@ import { useEffect } from "react"
 const useEditIncomingMailModalEffect = (formRef, letter)=>{
      useEffect(()=>{
           const form = formRef.current
-          form['agenda-number'].value = letter.agenda_number
-          form['letter-number'].value = letter.letter_number
-          form['source'].value = letter.source
-          form['letter-date'].value = letter.letter_date
-          form['received-date'].value = letter.received_date
-          form['recipient'].value = letter.recipient
-          form['subject'].value = letter.subject
+          form['letter-number'].value = letter?.letter_number
+          form['agenda-number'].value = letter?.agenda_number
+          form['source'].value = letter?.source
+          form['letter-date'].value = letter?.letter_date
+          form['received-date'].value = letter?.received_date
+          form['recipient'].value = letter?.recipient
+          form['subject'].value = letter?.subject
      }, [letter])
 }
 

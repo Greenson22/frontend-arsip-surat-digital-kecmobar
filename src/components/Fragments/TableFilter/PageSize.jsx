b1c4197aee36b844b74046e15e5da0d1ded0d02e
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-11 20:20:30 +0800

    menghapus props setCommand dan mengganti dengan commandSlice

--- src/components/Fragments/TableFilter/PageSize.jsx
+++ src/components/Fragments/TableFilter/PageSize.jsx
@@ -1,12 +1,15 @@
+import React from "react"
 import { usePageSizeSelect } from "../../../hooks"
+import { useDispatch } from "react-redux"
 
 const PageSize = (props)=>{
-     const {setCommand, api} = props
+     const {api} = props
+     const dispatch = useDispatch()
      return (
           <div className="col-12 col-md-2 d-flex">
                <sub className="mt-3">Show</sub>
                <div className="col-2 col-md-9 ms-1 me-1">
-                    <select className="form-select form-select-sm" name="" id="page_size" onClick={(event)=>{usePageSizeSelect(event, api, setCommand)}}>
+                    <select className="form-select form-select-sm" name="" id="page_size" onClick={(event)=>{usePageSizeSelect(event, api, dispatch)}}>
                          <option value="5">5</option>
                          <option value="10">10</option>
                          <option value="15">15</option>

0c2e92042e9fc6f238778b51d346d372524134bf
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-11 20:21:56 +0800

    menghapus props iData, command, dan setCommand serta mengganti dengan commandSlice dan dataSlice

--- src/components/Layouts/IncomingMailLayout.jsx
+++ src/components/Layouts/IncomingMailLayout.jsx
@@ -5,11 +5,14 @@ import {Table, TableHead} from '../Elements/Table'
 import {TitleBar, TableFilter, TableAction, IncomingMailTableBody, ExportModal, 
      AddIncomingMailModal, EditIncomingMailModal, DeleteIncomingMailModal, ViewMailModal} from '../Fragments'
 import { useSetPage } from "../../hooks"
+import { useDispatch, useSelector } from "react-redux"
 
 const IncomingMailLayout = (props)=>{
-     const api = import.meta.env.VITE_INCOMINGMAIL_API_KEY
-     const {data, iData, command, setCommand} = props
      const columns = ["No", "Nomor agenda", "Nomor surat", "Tanggal surat", "Tanggal terima", "Asal surat", "Perihal", "Penerima", "Tindakan"]
+     const {data} = props
+     const iData = useSelector(state=>state.dataSlice.iData)
+     const api = import.meta.env.VITE_INCOMINGMAIL_API_KEY
+     const dispatch = useDispatch()
 
      return (
           <div>
@@ -19,22 +22,21 @@ const IncomingMailLayout = (props)=>{
                          <TableAction title="Daftar surat masuk" button1="Tambah surat" button1_target="#addMailModal" button2="Ekspor" button2_target="#exportModal" />
                     </CardHeader>
                     <CardBody>
-                         <TableFilter api={api} setCommand={setCommand}/>
+                         <TableFilter api={api} />
                          <Table add_class="table-sm">
                               <TableHead columns={columns} />
-                              {data &&
-                                   <IncomingMailTableBody data={data['results']} setCommand={setCommand}></IncomingMailTableBody>}
+                              <IncomingMailTableBody data={data['results']} ></IncomingMailTableBody>
                          </Table>
-                         <MDBBtn onClick={() => { useSetPage(data['previous'], setCommand) } }>prev</MDBBtn>
-                         <MDBBtn onClick={() => { useSetPage(data['next'], setCommand) } }>next</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['previous'], dispatch) } }>prev</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['next'], dispatch) } }>next</MDBBtn>
                     </CardBody>
                </Card>
 
-               <EditIncomingMailModal letter={data['results'][iData]} setCommand={setCommand}/>
-               <ViewMailModal title='Surat masuk' command={command}/>
+               <EditIncomingMailModal letter={data['results'][iData]} />
+               <ViewMailModal title='Surat masuk'/>
                
-               <AddIncomingMailModal setCommand={setCommand}/>
-               <DeleteIncomingMailModal/>
+               <AddIncomingMailModal/>
+               {/* <DeleteIncomingMailModal/> */}
                <ExportModal title="Ekspor daftar surat masuk" />
           </div>
      )

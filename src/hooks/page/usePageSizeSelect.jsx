79875beaa849dc34ac829511b0c194e632200ed3
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-11 20:25:03 +0800

    menghapus setCommand dan mengganti ke stateManagement

--- src/hooks/page/usePageSizeSelect.jsx
+++ src/hooks/page/usePageSizeSelect.jsx
@@ -1,13 +1,15 @@
-const usePageSizeSelect = (event, url, setCommand)=> {
+import { setCommand } from "../../redux/slices/commandSlice"
+
+const usePageSizeSelect = (event, url, dispatch)=> {
      const urlSplit = url.split('?')
      const queryParams = new URLSearchParams(urlSplit[1])
 
      queryParams.set('page_size', event.target.value)
      const newUrl = urlSplit[0]+'?'+queryParams.toString()
-     setCommand({
+     dispatch(setCommand({
           'command': 'page_size',
           'url': newUrl
-     })
+     }))
 }
 
 export default usePageSizeSelect

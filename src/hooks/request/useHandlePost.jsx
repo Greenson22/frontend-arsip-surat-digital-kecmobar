27962bef7e69ff209fa348f3693c3f53e4ac9e90
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-14 01:21:18 +0800

    refactor : menambahkan menghapus field input setelah berhasil menginput data

--- src/hooks/request/useHandlePost.jsx
+++ src/hooks/request/useHandlePost.jsx
@@ -2,12 +2,15 @@ import usePostData from '../request/usePostData'
 import useAlert from '../alert/useAlert'
 import { setCommand } from '../../redux/slices/commandSlice'
 
-const useHandlePost = (url, token, data, dispatch)=>{
+const useHandlePost = (url, token, data, command, dispatch)=>{
      usePostData(url, data, token, 
      (response)=>{
           console.log(response)
           dispatch(setCommand(null))
           useAlert('success')
+
+          const form = document.getElementById(command.form_id)
+          form.reset()
      }, 
      (err)=>{
           useAlert('error')

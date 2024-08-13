aecc7923988a773e312db4512ea77d433af7deb1
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-13 13:07:28 +0800

    refactor : menghapus import useState

--- src/components/pages/incoming_mail.jsx
+++ src/components/pages/incoming_mail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { usePageEffect, useLoginValidate } from "../../hooks"
 import { IncomingMailEmptyLayout, IncomingMailLayout } from '../Layouts'
 import { useIncomingmailActions } from "../../hooks"

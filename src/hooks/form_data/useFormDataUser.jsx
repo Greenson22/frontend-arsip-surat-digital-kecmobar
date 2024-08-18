b3590a0d221152dec74136b3f368c0153c008021
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 14:32:13 +0800

    add : membuat form data untuk halaman user

---
+++ src/hooks/form_data/useFormDataUser.jsx
@@ -0,0 +1,17 @@
+const useFormDataUser = (id)=>{
+     const form = document.getElementById(id)
+     const formData = new FormData()
+     formData.append('username', form.querySelector('#username').value)
+     formData.append('password', form.querySelector('#password').value)
+     formData.append('first_name', form.querySelector('#first-name').value)
+     formData.append('last_name', form.querySelector('#last-name').value)
+     formData.append('email', form.querySelector('#email-address').value)
+     formData.append('is_active', form.querySelector('#active').checked)
+     formData.append('is_superuser', form.querySelector('#is-superuser').checked)
+     formData.append('phone_number', form.querySelector('#phone-number').value)
+     formData.append('photo_url', form.querySelector('#file').files[0])
+     
+     return formData
+}
+
+export default useFormDataUser

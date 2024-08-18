7d1503da47a1d8a93c57365c43b8f31f1120e740
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 18:23:24 +0800

    add : membuat form data untuk password change

---
+++ src/hooks/form_data/useFormDataPasswordChange.jsx
@@ -0,0 +1,30 @@
+const useFormDataPasswordChange = (command)=>{
+     const form = document.getElementById(command.form_id)
+
+     const oldPassword = form.querySelector('#old-password').value
+     const newPassword = form.querySelector('#new-password').value
+     const passwordConfirm = form.querySelector('#new-password-confirm').value
+     
+     const formData = new FormData()
+     formData.append('username', command.username)
+     formData.append('is_active', command.is_active)
+     formData.append('is_superuser', command.is_superuser)
+     
+     // --- Password Validation Logic ---
+     if (oldPassword) { // Check if old password field is not empty
+          if (newPassword !== passwordConfirm) { // Check if new password and confirm password match
+               useAlert('error_password_not_match')
+               return; // Stop submission if passwords don't match
+               }
+               formData.append('old_password', oldPassword)
+               formData.append('password', newPassword)
+          }
+     else if (newPassword || passwordConfirm) { 
+          useAlert('error_old_password_not_fill')
+          return; // Stop submission if old password is not provided but new passwords are
+     }
+
+     return formData
+}
+
+export default useFormDataPasswordChange

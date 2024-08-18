1036af5caf8933ab1175012f8999125d9f02fbb8
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 18:49:04 +0800

    refactor : mengubah underscore di id menjadi mines

--- src/hooks/form_data/useFormDataIncomingmail.jsx
+++ src/hooks/form_data/useFormDataIncomingmail.jsx
@@ -1,14 +1,17 @@
-const useFormDataIncomingmail = (id)=>{
-     const form = document.getElementById(id)
+const useFormDataIncomingmail = (command)=>{
+     const form = document.getElementById(command.form_id)
      const formData = new FormData()
-     formData.append('agenda_number', form.querySelector('#agenda_number').value)
-     formData.append('letter_number', form.querySelector('#letter_number').value)
+     formData.append('agenda_number', form.querySelector('#agenda-number').value)
+     formData.append('letter_number', form.querySelector('#letter-number').value)
      formData.append('source', form.querySelector('#source').value)
-     formData.append('letter_date', form.querySelector('#letter_date').value)
-     formData.append('received_date', form.querySelector('#received_date').value)
+     formData.append('letter_date', form.querySelector('#letter-date').value)
+     formData.append('received_date', form.querySelector('#received-date').value)
      formData.append('recipient', form.querySelector('#recipient').value)
      formData.append('subject', form.querySelector('#subject').value)
-     formData.append('file', form.querySelector('#document').files[0])
+
+     if (command.form_id != 'edit-incomingmail-form'){
+          formData.append('file', form.querySelector('#document').files[0])
+     }
      return formData
 }
 

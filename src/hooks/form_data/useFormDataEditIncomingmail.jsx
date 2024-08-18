a6dcda86c93292e37d80ef6705da673674e6df16
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 18:48:32 +0800

    refactor : menambahkan edit di akhiran id

--- src/hooks/form_data/useFormDataEditIncomingmail.jsx
+++ src/hooks/form_data/useFormDataEditIncomingmail.jsx
@@ -1,13 +1,13 @@
-const useFormDataEditIncomingmail = (id)=>{
-     const form = document.getElementById(id)
+const useFormDataEditIncomingmail = (command)=>{
+     const form = document.getElementById(command.form_id)
      const formData = new FormData()
-     formData.append('agenda_number', form.querySelector('#agenda-number').value)
-     formData.append('letter_number', form.querySelector('#letter-number').value)
-     formData.append('source', form.querySelector('#source').value)
-     formData.append('letter_date', form.querySelector('#letter-date').value)
-     formData.append('received_date', form.querySelector('#received-date').value)
-     formData.append('recipient', form.querySelector('#recipient').value)
-     formData.append('subject', form.querySelector('#subject').value)
+     formData.append('agenda_number', form.querySelector('#agenda-number-edit').value)
+     formData.append('letter_number', form.querySelector('#letter-number-edit').value)
+     formData.append('source', form.querySelector('#source-edit').value)
+     formData.append('letter_date', form.querySelector('#letter-date-edit').value)
+     formData.append('received_date', form.querySelector('#received-date-edit').value)
+     formData.append('recipient', form.querySelector('#recipient-edit').value)
+     formData.append('subject', form.querySelector('#subject-edit').value)
      return formData
 }
 

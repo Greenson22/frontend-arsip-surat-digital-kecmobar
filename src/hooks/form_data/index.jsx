e968fc5a75638f0fdd9b0fca93086d0b306b7436
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 19:30:54 +0800

    refactor : menghapus baris yang tidak perlu

--- src/hooks/form_data/index.jsx
+++ src/hooks/form_data/index.jsx
@@ -1,2 +1,3 @@
 export { default as useFormDataUser } from './useFormDataUser'
+export { default as useFormDataUserEdit } from './useFormDataUserEdit'
 export { default as useFormDataIncomingmail } from './useFormDataIncomingmail'

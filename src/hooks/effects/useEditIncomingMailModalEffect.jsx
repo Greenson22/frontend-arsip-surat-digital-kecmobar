59606150696e6d71cc164103be7d6fa44f82d8dc
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 18:48:07 +0800

    refactor : menambahkan edit di akhiran id

--- src/hooks/effects/useEditIncomingMailModalEffect.jsx
+++ src/hooks/effects/useEditIncomingMailModalEffect.jsx
@@ -3,13 +3,13 @@ import { useEffect } from "react"
 const useEditIncomingMailModalEffect = (formRef, letter)=>{
      useEffect(()=>{
           const form = formRef.current
-          form['letter-number'].value = letter?.letter_number
-          form['agenda-number'].value = letter?.agenda_number
-          form['source'].value = letter?.source
-          form['letter-date'].value = letter?.letter_date
-          form['received-date'].value = letter?.received_date
-          form['recipient'].value = letter?.recipient
-          form['subject'].value = letter?.subject
+          form['letter-number-edit'].value = letter?.letter_number
+          form['agenda-number-edit'].value = letter?.agenda_number
+          form['source-edit'].value = letter?.source
+          form['letter-date-edit'].value = letter?.letter_date
+          form['received-date-edit'].value = letter?.received_date
+          form['recipient-edit'].value = letter?.recipient
+          form['subject-edit'].value = letter?.subject
      }, [letter])
 }
 

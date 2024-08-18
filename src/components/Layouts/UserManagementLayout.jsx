e405d9b67b9af0f23dc384d106172364beaead9e
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-18 14:57:38 +0800

    refactor : beralih ke state management

--- src/components/Layouts/UserManagementLayout.jsx
+++ src/components/Layouts/UserManagementLayout.jsx
@@ -7,12 +7,14 @@ import { TitleBar, TableFilter, TableAction, UserManagementTableBody,
 
 import ViewProfileModal from "../Fragments/Modals/ViewProfileModal"
 import { useSetPage } from "../../hooks"
+import { useDispatch, useSelector } from "react-redux"
 
 const UserManagementLayout = (props)=>{
      const api = import.meta.env.VITE_USERS_API_KEY
-     const {data, iData, command, setCommand} = props
+     const {data} = props
+     const iData = useSelector(state => state.dataSlice.iData)
      const columns = ["No", "Nama", "Pengguna", "Tingkat", "Status", "Registrasi", "Tindakan"]
-
+     const dispatch = useDispatch()
 
      return (
           <div>
@@ -22,21 +24,21 @@ const UserManagementLayout = (props)=>{
                          <TableAction title="Daftar pengguna" button1="Tambah Pengguna" button1_target="#addUserModal" button2="Ekspor" button2_target="#exportModal"/>
                     </CardHeader>
                     <CardBody>
-                         <TableFilter setCommand={setCommand} api={api}/>
+                         <TableFilter api={api}/>
                          <Table add_class="table-sm">
                               <TableHead columns = {columns}/>
-                              <UserManagementTableBody data={data['results']} setCommand={setCommand}/>
+                              <UserManagementTableBody data={data['results']} />
                          </Table>
-                         <MDBBtn onClick={() => { useSetPage(data['previous'], setCommand) } }>prev</MDBBtn>
-                         <MDBBtn onClick={() => { useSetPage(data['next'], setCommand) } }>next</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['previous'], dispatch) } }>prev</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['next'], dispatch) } }>next</MDBBtn>
                     </CardBody>
                </Card>
 
                {/* Modal */}
-               <AddUserModal setCommand={setCommand}/>
-               <EditUserModal user={data['results'][iData]} setCommand={setCommand}/>
-               <ViewProfileModal user={data['results'][iData]} command={command} setCommand={command}/>
-               <ExportModal title="Ekspor daftar pengguna"/>
+               <AddUserModal />
+               {/* <EditUserModal user={data['results'][iData]} setCommand={setCommand}/> */}
+               {/* <ViewProfileModal user={data['results'][iData]} command={command} setCommand={command}/> */}
+               {/* <ExportModal title="Ekspor daftar pengguna"/> */}
           </div>
      )
 }

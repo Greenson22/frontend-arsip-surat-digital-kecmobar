d029b2ff73822d8535d1275548cf92759c82d7ed
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-19 21:03:23 +0800

    refactor : melihat informasi pengguna

--- src/components/Fragments/Modals/ViewProfileModal.jsx
+++ src/components/Fragments/Modals/ViewProfileModal.jsx
@@ -5,17 +5,18 @@ import { Modal, ModalHeader, ModalBody, ModalFooter } from "../../Elements/Modal
 import { Table, TableHead } from "../../Elements/Table"
 import { useFormattedDate } from '../../../hooks'
 import useViewProfileModalEffect from "../../../hooks/effects/useViewProfileModalEffect"
+import { useSelector } from "react-redux"
 
 
 
 const ViewProfileModal = (props)=>{
-   const {user, command} = props
+   const {user} = props
    const columns = ["Nama", "Username", "Superuser status", "Status", "Tanggal Register", "Nomor Telepon"]
    const imgRef = useRef()
    const datas = [user?.first_name+' '+user?.last_name, user?.username, (user?.is_superuser)?'aktif':'tidak aktif', (user?.is_active)?'aktif':'tidak aktif', useFormattedDate(user?.date_joined), user?.phone_number]
-   
+   const command = useSelector(state=>state.commandSlice.command)
    useViewProfileModalEffect(command, imgRef)
-
+   
    return(
          <Modal id="viewProfileModal">
             <ModalHeader title="Profil"/>
@@ -27,7 +28,6 @@ const ViewProfileModal = (props)=>{
             </ModalBody>
             <ModalFooter>
                <MDBBtn size="sm" data-bs-dismiss="modal" color="secondary">Tutup</MDBBtn>
-               <MDBBtn size="sm" color="warning">Simpan Perubahan</MDBBtn>
             </ModalFooter>
          </Modal>
      )

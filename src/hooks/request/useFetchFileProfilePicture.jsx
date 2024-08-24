8d9acf133370845b6b382a4d9ce62eb4917e80cf
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-24 22:25:46 +0800

    refactor : menyesuaikan endpoint

--- src/hooks/request/useFetchFileProfilePicture.jsx
+++ src/hooks/request/useFetchFileProfilePicture.jsx
@@ -4,7 +4,7 @@ const useFetchFileProfilePicture = (id, callbackResponse, callbackError)=>{
      const accessToken = localStorage.getItem('accessToken')
      const api = import.meta.env.VITE_USERS_API_KEY.split('?')[0]
     
-     useFetchFile(api+'user/'+id+'/profile_picture/', accessToken, callbackResponse, callbackError)
+     useFetchFile(api+id+'/profile_picture/', accessToken, callbackResponse, callbackError)
 }
 
 export default useFetchFileProfilePicture

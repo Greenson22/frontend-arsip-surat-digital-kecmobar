4228753641600e7222825ac2d4ec5d964edf94b4
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 20:55:17 +0800

    refactor : mengganti dengan state management

--- src/components/Fragments/Modals/EditOutgoingMailModal.jsx
+++ src/components/Fragments/Modals/EditOutgoingMailModal.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 
-import {Modal, ModalHeader, ModalBody, ModalFooter} from "../../Elements/Modal"
-
-import AnalisisIndicator from "../AnalisisIndicator"
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "../../Elements/Modal"
+import { useEditOutgoingMailModalEffect } from '../../../hooks/effects'
+import { useDispatch } from 'react-redux';
+import { setCommand } from '../../../redux/slices/commandSlice';
 
 const EditOutgoingMailModal = (props)=>{
      const {letter} = props
+     const formRef = useRef()
+     const dispatch = useDispatch()
+
+     const handleSubmit = (event)=>{
+          event.preventDefault()
+          dispatch(
+               setCommand({
+                    'command' : 'put',
+                    'id' : letter.id,
+                    'form_id' : 'edit-outgoingmail-form'
+               })
+          )
+     }
+     useEditOutgoingMailModalEffect(formRef, letter)
+     
      return(
           <Modal id="edit-outgoingmail-modal">
                <ModalHeader title="Ubah surat keluar"/>
                <ModalBody>
-                    <form action="">
+                    <form action="" id='edit-outgoingmail-form' ref={formRef} onSubmit={handleSubmit}>
                          <div className="row">
                               <div className="col-6">
                                    <MDBInput id='agenda-number-edit' label="Nomor agenda" type="text" className='mb-2' defaultValue={' '}/>
                               </div>
                               <div className="col-6">
-                                   <MDBInput label="letter-date-edit" type="text" className='mb-2' defaultValue={' '}/>
+                                   <MDBInput id="letter-date-edit" label='Tanggal surat' type="text" className='mb-2' defaultValue={' '}/>
                               </div>
                          </div>
-                         <MDBInput label="letter-number-edit" type="text" className='mb-2' defaultValue={' '}/>
-                         <MDBInput label="destination-edit" type="text" className='mb-2' defaultValue={' '}/>
-                         <MDBInput label="subject-edit" type="text" className='mb-2' defaultValue={' '}/>
+                         <MDBInput id="letter-number-edit" label='Nomor surat' type="text" className='mb-2' defaultValue={' '}/>
+                         <MDBInput id="destination-edit" label='Tujuan surat' type="text" className='mb-2' defaultValue={' '}/>
+                         <MDBInput id="subject-edit" label='Perihal' type="text" className='mb-2' defaultValue={' '}/>
                     </form>
                </ModalBody>
                <ModalFooter>
                     <MDBBtn size='sm' color='secondary' data-bs-dismiss="modal">Tutup</MDBBtn>
-                    <MDBBtn size='sm' color='primary' disabled>Simpan perubahan</MDBBtn>
+                    <MDBBtn size='sm' color='primary' form='edit-outgoingmail-form'>Simpan perubahan</MDBBtn>
                </ModalFooter>
           </Modal>
      )

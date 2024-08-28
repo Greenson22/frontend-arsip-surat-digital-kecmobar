6ade075873a10f2f60cf7af101de560742d28203
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 18:48:41 +0800

    add : index untuk folder url

---
+++ src/hooks/url/index.jsx
@@ -0,0 +1,4 @@
+export { default as useUrlModifier } from './useUrlModifier'
+export { default as useObjectURL } from './useObjectURL'
+export { default as useUrlParams } from './useUrlParams'
+export { default as useUrlSyn } from './useUrlSyn'

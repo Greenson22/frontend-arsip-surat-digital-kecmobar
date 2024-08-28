d361fa7614783b6a36cdcb0132451f0e5fb7c496
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 19:49:47 +0800

    add : index untuk folder effects di hooks

---
+++ src/hooks/effects/index.jsx
@@ -0,0 +1,5 @@
+export { default as useEditOutgoingMailModalEffect } from './useEditOutgoingMailModalEffect'
+export { default as useEditIncomingMailModalEffect } from './useEditIncomingMailModalEffect'
+export { default as useContainerLayoutEffect } from './useContainerLayoutEffect'
+export { default as useEditUserModalEffect } from './useEditUserModalEffect'
+export { default as useViewProfileModalEffect } from './useViewProfileModalEffect'

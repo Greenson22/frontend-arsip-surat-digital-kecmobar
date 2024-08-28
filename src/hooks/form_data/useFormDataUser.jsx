3719d067d9cef646bca739ad673c4b352dba651d
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 22:16:21 +0800

    refactor : menambahkan formData untuk edit user

--- src/hooks/form_data/useFormDataUser.jsx
+++ src/hooks/form_data/useFormDataUser.jsx
@@ -1,15 +1,27 @@
 const useFormDataUser = (command)=>{
      const form = document.getElementById(command.form_id)
      const formData = new FormData()
-     formData.append('username', form.querySelector('#username').value)
-     formData.append('password', form.querySelector('#password').value)
-     formData.append('first_name', form.querySelector('#first-name').value)
-     formData.append('last_name', form.querySelector('#last-name').value)
-     formData.append('email', form.querySelector('#email-address').value)
-     formData.append('is_active', form.querySelector('#active').checked)
-     formData.append('is_superuser', form.querySelector('#is-superuser').checked)
-     formData.append('phone_number', form.querySelector('#phone-number').value)
-     formData.append('photo_url', form.querySelector('#file').files[0])
+
+     if (command.form_id == 'edit-usermanagement-modal'){
+          formData.append('username', form['username-edit'].value)
+          formData.append('first_name', form['first-name-edit'].value)
+          formData.append('last_name', form['last-name-edit'].value)
+          formData.append('email', form['email-address-edit'].value)
+          formData.append('is_active', form['is-active-edit'].checked)
+          formData.append('is_superuser', form['is-superuser-edit'].checked)
+          formData.append('phone_number', form['phone-number-edit'].value)
+          
+          // cek jika password di masukan
+          const password = form['new-password-edit'].value
+          if (password){
+               formData.append('password', form['new-password-edit'].value)
+          }
+          // cek jika jika foto di input
+          const picFile = form['file-edit'].files[0]
+          if (picFile){
+               formData.append('photo_url', picFile)
+          }
+     }
      
      return formData
 }

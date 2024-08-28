c2f6dc6b7e3273f3d2246b20bc61511dd1d5d892
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 18:55:07 +0800

    add : layout untuk outgoingmail

---
+++ src/components/Layouts/OutgoingMailLayout.jsx
@@ -0,0 +1,38 @@
+import { MDBCard, MDBCardHeader, MDBCardBody, MDBBtn, MDBTable } from "mdb-react-ui-kit"
+import { TitleBar, TableAction, TableFilter, OutgoingMailTableBody, AddOutgoingMailModal, ViewMailModal } from "../Fragments"
+import { TableHead } from "../Elements/Table"
+import { useDispatch, useSelector } from "react-redux"
+import { useSetPage } from "../../hooks"
+
+const OutgoingMailLayout = (props)=>{
+     const columns = ["No", "Nomor agenda", "Tanggal surat", "Tujuan surat", "Nomor surat", "Perihal", "Tindakan"]
+     const {data} = props
+     const iData = useSelector(state=>state.dataSlice.iData)
+     const api = import.meta.env.VITE_OUTGOINGMAIL_API_KEY
+     const dispatch = useDispatch()
+
+     return (
+          <div>
+               <TitleBar>Surat Keluar</TitleBar>
+               <MDBCard>
+                    <MDBCardHeader>
+                         <TableAction title="Daftar surat keluar" button1="Tambah surat" button1_target="#add-outgoingmail-modal" button2="Ekspor" button2_target="#export-modal" />
+                    </MDBCardHeader>
+                    <MDBCardBody>
+                         <TableFilter api={api}/>
+                         <MDBTable small bordered striped>
+                              <TableHead columns={columns}></TableHead>
+                              <OutgoingMailTableBody data={data['results']}/>
+                         </MDBTable>
+                         <MDBBtn onClick={() => { useSetPage(data['previous'], dispatch) } }>prev</MDBBtn>
+                         <MDBBtn onClick={() => { useSetPage(data['next'], dispatch) } }>next</MDBBtn>
+                    </MDBCardBody>
+               </MDBCard>
+
+               <AddOutgoingMailModal />
+               <ViewMailModal title='Surat Keluar' api={api}/>
+          </div>
+     )
+}
+
+export default OutgoingMailLayout

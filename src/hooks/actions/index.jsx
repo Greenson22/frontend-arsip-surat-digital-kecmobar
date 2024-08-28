104c29b07353e552f92e9c1c296597b6be2dbffb
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 20:54:37 +0800

    add : index untuk folder actions di hooks

---
+++ src/hooks/actions/index.jsx
@@ -0,0 +1,4 @@
+export { default as useHomeActions } from './useHomeActions'
+export { default as useIncomingmailActions } from './useIncomingmailActions'
+export { default as useOutgoingmailAction } from './useOutgoingmailAction'
+export { default as useUserManagementActions } from './useUserManagementActions'

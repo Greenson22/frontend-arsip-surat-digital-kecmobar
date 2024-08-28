cdb91592b550849b97b249411eee325767e7ab57
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 18:57:18 +0800

    add : action untuk Outgoingmail

---
+++ src/hooks/actions/useOutgoingmailAction.jsx
@@ -0,0 +1,51 @@
+import { useFormDataOutgoingmail } from '../form_data'
+import { useHandlePost, useHandleFetch } from '../request'
+import { usePaginationLocalStorage } from "../pagination"
+import useResponseFormattedString from '../useResponseFormattedString'
+import useErrorAlert from '../alert/useErrorAlert'
+import { useUrlSyn } from '../url'
+
+const useOutgoingmailAction = (command, dispatch)=>{
+     const url = import.meta.env.VITE_OUTGOINGMAIL_API_KEY
+     const token = localStorage.getItem('accessToken')
+     if (command){
+          switch(command.command){
+               case 'post':
+                    const newUrl = url.split('?')[0]
+                    const data = useFormDataOutgoingmail(command)
+                    useHandlePost(newUrl, token, data, command, dispatch, (error)=>{
+                         useErrorAlert('add_outgoingmail', useResponseFormattedString(error.response.data))
+                    })
+                    break
+               // case 'put':
+               //      const putData = useFormDataEditIncomingmail(command)
+               //      const newPutUrl = useUrlModifier(url, command)
+               //      useHandlePut(newPutUrl, token, putData, dispatch)
+               //      break
+               // case 'delete':
+               //      useHandleDelete(url, token, command, dispatch)
+               //      break
+               case 'view_data':
+                    dispatch(setIData(command.index))
+                    break
+               case 'navigation':
+                    useHandleFetch(command.navigation_link, token, dispatch)
+                    usePaginationLocalStorage(command.navigation_link)
+                    break
+               case 'page_size':
+                    useHandleFetch(command.url, token, dispatch)
+                    usePaginationLocalStorage(command.url)
+                    break
+               case 'search':
+                    const urlSyn = useUrlSyn(url, 'pagination')
+                    const searchUrl = urlSyn+'&search='+command.words
+                    useHandleFetch(searchUrl, token, dispatch)
+                    break
+          }
+     }else{
+          useHandleFetch(url, token, dispatch)
+          usePaginationLocalStorage(url)
+     }
+}
+
+export default useOutgoingmailAction

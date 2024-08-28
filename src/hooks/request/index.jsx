f594fb1fa938311e5091af68cd4684867630c715
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 19:34:23 +0800

    refactor : menambahkan baris useHandleDelete

--- src/hooks/request/index.jsx
+++ src/hooks/request/index.jsx
@@ -1,2 +1,3 @@
 export { default as useHandlePost } from './useHandlePost'
 export { default as useHandleFetch } from './useHandleFetch'
+export { default as useHandleDelete } from './useHandleDelete'

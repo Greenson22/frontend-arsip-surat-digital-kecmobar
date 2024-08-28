6466a76de6f42d1f6fdd8e0cdec26d507ea59f13
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 18:52:26 +0800

    refactor : menambahkan baris OutgoingMailTableBody

--- src/components/Fragments/index.jsx
+++ src/components/Fragments/index.jsx
@@ -2,6 +2,7 @@ export { default as TitleBar } from './TitleBar'
 export { default as TableFilter } from './TableFilter'
 export { default as TableAction } from "./TableAction"
 export { default as IncomingMailTableBody } from './IncomingMailTableBody'
+export { default as OutgoingMailTableBody } from './OutgoingMailTableBody'
 export { default as UserManagementTableBody } from './UserManagementTableBody'
 export { default as ProfileBox } from './ProfileBox'
 export { default as UserBox } from './UserBox'

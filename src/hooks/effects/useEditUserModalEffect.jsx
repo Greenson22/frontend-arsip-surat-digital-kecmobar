7b7b5be8be9f83c6aba7730ec2262c45f584dff6
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 22:58:11 +0800

    refactor : menambahkan tanda tanya ke akhir user

--- src/hooks/effects/useEditUserModalEffect.jsx
+++ src/hooks/effects/useEditUserModalEffect.jsx
@@ -2,13 +2,13 @@ import { useEffect } from "react"
 
 const useEditUserModalEffect = (user, formRef)=>{
      useEffect(()=>{
-          formRef.current['username-edit'].value = user.username
-          formRef.current['first-name-edit'].value = user.first_name
-          formRef.current['last-name-edit'].value = user.last_name
-          formRef.current['email-address-edit'].value = user.email
-          formRef.current['is-active-edit'].checked = user.is_active
-          formRef.current['is-superuser-edit'].checked = user.is_superuser
-          formRef.current['phone-number-edit'].value = user.phone_number
+          formRef.current['username-edit'].value = user?.username
+          formRef.current['first-name-edit'].value = user?.first_name
+          formRef.current['last-name-edit'].value = user?.last_name
+          formRef.current['email-address-edit'].value = user?.email
+          formRef.current['is-active-edit'].checked = user?.is_active
+          formRef.current['is-superuser-edit'].checked = user?.is_superuser
+          formRef.current['phone-number-edit'].value = user?.phone_number
      }, [user])
 }
 

a14cb69823804f505db596adee55edb7397e972d
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 20:57:51 +0800

    refactor : menambahkan jika edit-form atau tidak

--- src/hooks/form_data/useFormDataIncomingmail.jsx
+++ src/hooks/form_data/useFormDataIncomingmail.jsx
@@ -1,16 +1,23 @@
 const useFormDataIncomingmail = (command)=>{
      const form = document.getElementById(command.form_id)
      const formData = new FormData()
-     formData.append('agenda_number', form.querySelector('#agenda-number').value)
-     formData.append('letter_number', form.querySelector('#letter-number').value)
-     formData.append('source', form.querySelector('#source').value)
-     formData.append('letter_date', form.querySelector('#letter-date').value)
-     formData.append('received_date', form.querySelector('#received-date').value)
-     formData.append('recipient', form.querySelector('#recipient').value)
-     formData.append('subject', form.querySelector('#subject').value)
-
      if (command.form_id != 'edit-incomingmail-form'){
           formData.append('file', form.querySelector('#document').files[0])
+          formData.append('agenda_number', form.querySelector('#agenda-number').value)
+          formData.append('letter_number', form.querySelector('#letter-number').value)
+          formData.append('source', form.querySelector('#source').value)
+          formData.append('letter_date', form.querySelector('#letter-date').value)
+          formData.append('received_date', form.querySelector('#received-date').value)
+          formData.append('recipient', form.querySelector('#recipient').value)
+          formData.append('subject', form.querySelector('#subject').value)
+     }else{
+          formData.append('agenda_number', form.querySelector('#agenda-number-edit').value)
+          formData.append('letter_number', form.querySelector('#letter-number-edit').value)
+          formData.append('source', form.querySelector('#source-edit').value)
+          formData.append('letter_date', form.querySelector('#letter-date-edit').value)
+          formData.append('received_date', form.querySelector('#received-date-edit').value)
+          formData.append('recipient', form.querySelector('#recipient-edit').value)
+          formData.append('subject', form.querySelector('#subject-edit').value)
      }
      return formData
 }
--- src/hooks/form_data/useFormDataOutgoingmail.jsx
+++ src/hooks/form_data/useFormDataOutgoingmail.jsx
@@ -2,14 +2,19 @@ const useFormDataOutgoingmail = (command)=>{
      const form = document.getElementById(command.form_id)
      const formData = new FormData()
      
-     formData.append('agenda_number', form['agenda-number'].value)
-     formData.append('letter_number', form['letter-number'].value)
-     formData.append('letter_date', form['letter-date'].value)
-     formData.append('destination', form['destination'].value)
-     formData.append('subject', form['subject'].value)
-
      if (command.form_id != 'edit-outgoingmail-form'){
+          formData.append('agenda_number', form['agenda-number'].value)
+          formData.append('letter_number', form['letter-number'].value)
+          formData.append('letter_date', form['letter-date'].value)
+          formData.append('destination', form['destination'].value)
+          formData.append('subject', form['subject'].value)
           formData.append('file', form['document'].files[0])
+     }else{
+          formData.append('agenda_number', form['agenda-number-edit'].value)
+          formData.append('letter_number', form['letter-number-edit'].value)
+          formData.append('letter_date', form['letter-date-edit'].value)
+          formData.append('destination', form['destination-edit'].value)
+          formData.append('subject', form['subject-edit'].value)
      }
      return formData
 }

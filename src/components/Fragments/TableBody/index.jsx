b2de9fb4b6ce5f814f904672ff1635eebbbab69a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 19:33:16 +0800

    add : index untuk folder TableBody

---
+++ src/components/Fragments/TableBody/index.jsx
@@ -0,0 +1,4 @@
+export { default as UserBoxTableBody } from './UserBoxTableBody'
+export { default as OutgoingMailTableBody } from './OutgoingMailTableBody'
+export { default as IncomingMailTableBody } from './IncomingMailTableBody'
+export { default as UserManagementTableBody } from './UserManagementTableBody'

68cec39ca20ca081a992acfae1ade707d9b1cfc3
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-28 19:47:36 +0800

    refactor : menambahkan AddOutgoingMailModal dan ViewMailModal

--- src/components/Fragments/Modals/index.jsx
+++ src/components/Fragments/Modals/index.jsx
@@ -1,1 +1,3 @@
+export { default as AddOutgoingMailModal } from './AddOutgoingMailModal'
 export { default as EditOutgoingMailModal } from './EditOutgoingMailModal'
+export { default as ViewMailModal } from './ViewMailModal'

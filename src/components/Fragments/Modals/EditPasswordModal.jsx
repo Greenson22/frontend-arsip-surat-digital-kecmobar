5fdd1df844693f0862ec567c370c23b11bb41a6c
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-29 17:54:16 +0800

    refactor : menambahkan show password

--- src/components/Fragments/Modals/EditPasswordModal.jsx
+++ src/components/Fragments/Modals/EditPasswordModal.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import {Modal, ModalHeader, ModalBody, ModalFooter} from "../../Elements/Modal"
-import { MDBInput, MDBBtn } from "mdb-react-ui-kit"
+import { MDBInput, MDBBtn, MDBCheckbox } from "mdb-react-ui-kit"
 import { useDispatch } from "react-redux"
 import { setCommand } from "../../../redux/slices/commandSlice"
 
@@ -21,6 +21,23 @@ const EditPasswordModal = (props)=>{
           }));
      }
 
+     const handleBtnShowPassCheck = (event)=>{
+          const form = document.getElementById('form-edit-password')
+          const oldPassword = form['old-password']
+          const newPassword = form['new-password']
+          const newPasswordConfirm = form['new-password-confirm']
+          
+          if (event.target.checked){
+               oldPassword.type = 'text'
+               newPassword.type = 'text'
+               newPasswordConfirm.type = 'text'
+          }else{
+               oldPassword.type = 'password'
+               newPassword.type = 'password'
+               newPasswordConfirm.type = 'password'
+          }
+     }
+
      return(
           <Modal id="editPasswordModal">
                <ModalHeader title="Ubah Password"/>
@@ -28,7 +45,8 @@ const EditPasswordModal = (props)=>{
                     <form action="" onSubmit={onHandleSubmit} id="form-edit-password">
                          <MDBInput label="Password lama" id="old-password" type="password" className='mb-2' required/>
                          <MDBInput label="Password baru" id="new-password" type="password" className='mb-2' required/>
-                         <MDBInput label="Konfirmasi password baru" id="new-password-confirm" type="password" className='mb-2' required/>
+                         <MDBInput label="Konfirmasi password baru" id="new-password-confirm" type="password" className='mb-2' required></MDBInput>
+                         <MDBCheckbox label="Show password" onClick={handleBtnShowPassCheck}/>
                     </form>
                </ModalBody>
                <ModalFooter>

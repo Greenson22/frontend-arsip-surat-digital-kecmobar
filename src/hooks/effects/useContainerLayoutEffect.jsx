f70c9c1f3176f7e919c9e17dbb8c87fd7404ecae
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-30 00:15:08 +0800

    refactor : mengeluarkan jwt accessToken ke luar axios response

--- src/hooks/effects/useContainerLayoutEffect.jsx
+++ src/hooks/effects/useContainerLayoutEffect.jsx
@@ -4,20 +4,20 @@ import { useEffect } from "react"
 
 const useContainerLayoutEffect = (navRef)=>{
   useEffect(()=>{
-          function handleAxiosResponse(response){
-            const namaBulan = [ "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" ]
-            const unixTime = response.data.unixtime
-            const dateTime = new Date(unixTime*1000)
-            
-            const jwt = jwtDecode(localStorage.getItem('accessToken'))
-            navRef.current.querySelector("#date").textContent = dateTime.getDate()+' '+namaBulan[dateTime.getMonth()]+' '+dateTime.getFullYear()
-            navRef.current.querySelector('#user-name').textContent = jwt.first_name+' '+jwt.last_name
-          }
-          function handleAxiosError(err){
-            console.log(err)
-          }
-          axios.get(import.meta.env.VITE_WORLD_TIME_MAKASAR_API_KEY).then(handleAxiosResponse).catch(handleAxiosError)
-        }, [])
+    const jwt = jwtDecode(localStorage.getItem('accessToken'))
+    navRef.current.querySelector('#user-name').textContent = jwt.first_name+' '+jwt.last_name
+    function handleAxiosResponse(response){
+      const namaBulan = [ "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" ]
+      const unixTime = response.data.unixtime
+      const dateTime = new Date(unixTime*1000)
+      
+      navRef.current.querySelector("#date").textContent = dateTime.getDate()+' '+namaBulan[dateTime.getMonth()]+' '+dateTime.getFullYear()
+    }
+    function handleAxiosError(err){
+      console.log(err)
+    }
+    axios.get(import.meta.env.VITE_WORLD_TIME_MAKASAR_API_KEY).then(handleAxiosResponse).catch(handleAxiosError)
+  }, [])
 }
 
 export default useContainerLayoutEffect

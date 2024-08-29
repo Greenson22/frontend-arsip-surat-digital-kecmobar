81c0566c811486fa788b50d0082b8a4739374d52
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-30 00:03:04 +0800

    add : untuk melakukan crop image

---
+++ src/components/Fragments/CropImage.jsx
@@ -0,0 +1,26 @@
+import { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
+import profile from '../../assets/images/avatar_female.jpeg'
+
+const CropImage = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+
+  const onCropComplete = (croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels)
+  }
+
+  return (
+    <Cropper
+      image={profile}
+      crop={crop}
+      zoom={zoom}
+      aspect={4 / 3}
+      onCropChange={setCrop}
+      onCropComplete={onCropComplete}
+      onZoomChange={setZoom}
+    />
+  )
+}
+
+export default CropImage
---
+++ src/components/Fragments/Modals/PictureCroppingModal.jsx
@@ -0,0 +1,10 @@
+import {Modal, ModalHeader, ModalBody, ModalFooter} from "../../Elements/Modal"
+
+const PictureCroppingModal = ()=>{
+     return (
+          <Modal id="">
+               <ModalHeader title='Cropping Image'/>
+               
+          </Modal>
+     )
+}

a5984ec0e21c355792d354aaa97b3f4a5cc5d40f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-02 22:39:50 +0800

    reafactor : bisa mematikan atau menghidupkan

--- src/components/Fragments/AnalisisIndicator.jsx
+++ src/components/Fragments/AnalisisIndicator.jsx
@@ -1,12 +1,13 @@
-const AnalisisIndicator = ()=>{
-     return (
+const AnalisisIndicator = (props)=>{
+     const {isActive} = props
+     return isActive ? (
           <div className="d-flex">
                <div className="spinner-grow spinner-grow-sm text-primary mt-4"></div>
                <div className="mt-4 ms-2">
                     <span className="text-animation text-primary">Proses menganalisa file...</span>
                </div>
           </div>
-     )
+     ):(<div></div>)
 }
 
 export default AnalisisIndicator

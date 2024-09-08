eb930ff8bf31b612053839bef196d8e1154917c1
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-08 17:39:26 +0800

    refactor : menambahkan list cloud

--- src/components/Fragments/List/ListGroup.jsx
+++ src/components/Fragments/List/ListGroup.jsx
@@ -4,11 +4,15 @@ import List from "./List"
 
 const listGroup = (props)=>{
      const {fileNote} = props
+     // console.log(fileNote)
      return (
           <MDBListGroup>
                {
                     fileNote.map((file, index)=>{
-                         if (file.loading === true) { //sementara melakukan analisis
+                         if (file.cloud === true){ //jika sudah di upload
+                              return <List status={'cloud'} key={index}>{file.name}</List>
+                         }
+                         else if (file.loading === true) { //sementara melakukan analisis
                               return <List status={'spinner'} key={index}>{file.name}</List>
                          }else if (file.entities===null){ //jika entities masih null
                               return <List status={''} key={index}>{file.name}</List>

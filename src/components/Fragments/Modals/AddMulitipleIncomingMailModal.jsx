12e69853e499a708e91e360345710052ca10f5be
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-08 18:38:14 +0800

    refactor : menyederhanakan lagi file note dengan menggunakan status dan bukan lagi loading atau yang lain-lain terpisa

--- src/components/Fragments/Modals/AddMulitipleIncomingMailModal.jsx
+++ src/components/Fragments/Modals/AddMulitipleIncomingMailModal.jsx
@@ -6,46 +6,50 @@ import { usePostData } from "../../../hooks"
 import { useDispatch } from "react-redux"
 import { setCommand } from "../../../redux/slices/commandSlice"
 
-const AddMultipleIncomingMailModal = ()=>{
+const AddMultipleIncomingMailModal = () => {
      const [fileList, setFileList] = useState(null)
      const [fileNote, setFileNote] = useState(null)
      const inputFileRef = useRef()
      const dispatch = useDispatch()
-     
-     const handleInputFileChange = (event)=>{
+
+     const handleInputFileChange = (event) => {
           const files = Array.from(event.target.files)
           const notes = []
-          files.map((file, index)=>{
+          files.map((file, index) => {
                file.entities = null
                notes.push({
-                    'name' : file.name,
-                    'entities' : null,
-                    'loading' : false,
-                    'cloud' : false,
+                    'name': file.name,
+                    'entities': null,
+                    'status': null,
                })
           })
           setFileList([...files])
           setFileNote([...notes])
           console.log('file list dibuat dengan entities kosong')
      }
-     const handleBtnAnalysClick = (event)=>{
+     const handleBtnAnalysClick = (event) => {
           const duplicateFileNote = fileNote.slice()
 
-          fileList.map((file, index)=>{
+          fileList.map((file, index) => {
                const formData = new FormData()
                formData.append('file', fileList[index])
                console.log('sedang menganalisa beberapa file')
 
-               duplicateFileNote[index].loading = true
+               duplicateFileNote[index].status = 'loading'
                setFileNote(duplicateFileNote)
 
-               usePostData(import.meta.env.VITE_GENAI_API_KEY, formData, localStorage.getItem('accessToken'), (response)=>{
+               usePostData(import.meta.env.VITE_GENAI_API_KEY, formData, localStorage.getItem('accessToken'), (response) => {
                     const duplicateFileNoteResponse = fileNote.slice()
                     const data = response.data
                     duplicateFileNoteResponse[index].entities = data.entities
-                    duplicateFileNoteResponse[index].loading = false
+                    duplicateFileNoteResponse[index].status = 'check'
                     setFileNote(duplicateFileNoteResponse)
                     console.log('satu proses selesai')
+               }, (error)=>{
+                    const duplicateFileNoteResponse = fileNote.slice()
+                    duplicateFileNote[index].status = 'fail'
+                    setFileNote(duplicateFileNoteResponse)
+                    console.log('satu proses gagal')
                })
           })
      }
@@ -58,16 +62,16 @@ const AddMultipleIncomingMailModal = ()=>{
           }))
      }
      return (
-          <Modal id="add-multiple-mail-modal"> 
-               <ModalHeader title='Tambah beberapa surat'/>
+          <Modal id="add-multiple-mail-modal">
+               <ModalHeader title='Tambah beberapa surat' />
                <ModalBody>
                     <MDBInputGroup className="mb-2">
                          <MDBInput id="input-file" type="file" multiple ref={inputFileRef} accept="application/pdf" onChange={handleInputFileChange}></MDBInput>
                          <MDBBtn size="sm" onClick={handleBtnAnalysClick}>Analisa</MDBBtn>
                     </MDBInputGroup>
-                    
+
                     {fileNote != null && <ListGroup fileNote={fileNote} />}
-               </ModalBody> 
+               </ModalBody>
                <ModalFooter>
                     <MDBBtn size='sm' color='secondary' data-bs-dismiss="modal">Tutup</MDBBtn>
                     <MDBBtn size='sm' color='primary' data-bs-dismiss="modal" onClick={handleBtnPostClick}>Tambah semua surat</MDBBtn>

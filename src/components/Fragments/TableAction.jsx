93d6d64d89306214e0eb3a4b0197123408ce15a5
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:23:57 +0800

    refactor : menghapus button export

--- src/components/Fragments/TableAction.jsx
+++ src/components/Fragments/TableAction.jsx
@@ -2,20 +2,14 @@ import React, { useEffect, useRef } from "react"
 import { MDBBtn } from "mdb-react-ui-kit"
 
 const TableAction = (props)=>{
-     const {title, button1, button1_target, button2, button2_target, button2Disabled=false, buttonAClick} = props
-     const btnExportRef = useRef()
-
-     useEffect(()=>{
-          btnExportRef.current.disabled = button2Disabled
-     }, [])
+     const {title, buttonChildren, buttonClick} = props
 
      return(
           <div className="row">
                <div className="col-12">
                     <h5 className="card-title mt-1 float-start">{title}</h5>
                     <div className="btn-group float-end">
-                         <MDBBtn outline data-bs-target={button1_target} onClick={buttonAClick}>{button1}</MDBBtn>
-                         <MDBBtn color="success" outline  data-bs-toggle="modal" ref={btnExportRef} data-bs-target={button2_target}>{button2}</MDBBtn>
+                         <MDBBtn outline onClick={buttonClick}>{buttonChildren}</MDBBtn>
                     </div>
                </div>
           </div>

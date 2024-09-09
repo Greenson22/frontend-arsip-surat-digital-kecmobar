bddffd9ad24a3ecf6a2295d11a5c5c9c1ba52ccb
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:11:22 +0800

    refactor : menambahkan add mutliple incomingmail modal

--- src/components/Layouts/IncomingMailLayout.jsx
+++ src/components/Layouts/IncomingMailLayout.jsx
@@ -2,11 +2,12 @@ import { MDBBtn } from "mdb-react-ui-kit"
 
 import { Card , CardHeader, CardBody} from "../Elements/Card"
 import {Table, TableHead} from '../Elements/Table'
-import {TitleBar, TableFilter, TableAction, IncomingMailTableBody, ExportModal, 
-     AddIncomingMailModal, EditIncomingMailModal, ViewMailModal} from '../Fragments'
+import {TitleBar, TableFilter, TableAction, IncomingMailTableBody, ExportModal} from '../Fragments'
+import { AddIncomingmailModalV2, EditIncomingMailModal, ViewMailModal} from '../Fragments/Modals'
 import { useSetPage } from "../../hooks"
 import { useDispatch, useSelector } from "react-redux"
 import AddMultipleIncomingMailModal from "../Fragments/Modals/AddMulitipleIncomingMailModal"
+import { useState } from "react"
 
 const IncomingMailLayout = (props)=>{
      const columns = ["No", "Nomor agenda", "Nomor surat", "Tanggal surat", "Tanggal terima", "Asal surat", "Perihal", "Penerima", "Tindakan"]
@@ -14,13 +15,15 @@ const IncomingMailLayout = (props)=>{
      const iData = useSelector(state=>state.dataSlice.iData)
      const api = import.meta.env.VITE_INCOMINGMAIL_API_KEY
      const dispatch = useDispatch()
+     const [addModal, setAddModal] = useState(false)
+     const [addMultipleModal, setMultipleModal] = useState(false)
 
      return (
           <div>
                <TitleBar>Surat masuk</TitleBar>
                <Card>
                     <CardHeader>
-                         <TableAction title="Daftar surat masuk" button1="Tambah surat" button1_target="#add-mail-modal" button2="Ekspor" button2_target="#exportModal" />
+                    <TableAction title="Daftar surat masuk" button1="Tambah surat" button2="Ekspor" button2_target="#exportModal" button2Disabled={false} buttonAClick={()=>{setAddModal(true)}}/>
                     </CardHeader>
                     <CardBody>
                          <TableFilter api={api} />
@@ -36,9 +39,8 @@ const IncomingMailLayout = (props)=>{
                <EditIncomingMailModal letter={data['results'][iData]} />
                <ViewMailModal title='Surat masuk' api={api}/>
                
-               <AddIncomingMailModal/>
-               <AddMultipleIncomingMailModal/>
-               {/* <DeleteIncomingMailModal/> */}
+               <AddIncomingmailModalV2 addModal={addModal} setAddModal={setAddModal} />
+               <AddMultipleIncomingMailModal addMultipleModal={addMultipleModal} setMultipleModal={setMultipleModal} />
                <ExportModal title="Ekspor daftar surat masuk" />
           </div>
      )

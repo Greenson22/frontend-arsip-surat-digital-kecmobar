582e0d7a6d7f5501df36ea3ed30a585d6685b7ec
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:11:58 +0800

    refactor : mengaktifkan dispatch

--- src/hooks/handle/add_incomingmail_modal/useHandleSubmit.jsx
+++ src/hooks/handle/add_incomingmail_modal/useHandleSubmit.jsx
@@ -2,10 +2,10 @@ import { setCommand } from "../../../redux/slices/commandSlice"
 
 const useHandleSubmit = (event, dispatch)=>{
      event.preventDefault()
-     // dispatch(setCommand({
-     //      'command': 'post',
-     //      'form_id' : 'add-incomingmail-form'
-     // }))
+     dispatch(setCommand({
+          'command': 'post',
+          'form_id' : 'add-incomingmail-form'
+     }))
 }
 
 export default useHandleSubmit

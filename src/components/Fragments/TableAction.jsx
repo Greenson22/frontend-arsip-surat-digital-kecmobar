61830c224b72e8bd7bedf893a3fb360a607fb96f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:09:42 +0800

    refactor : merubah cara memanggil modal

--- src/components/Fragments/TableAction.jsx
+++ src/components/Fragments/TableAction.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react"
 import { MDBBtn } from "mdb-react-ui-kit"
 
 const TableAction = (props)=>{
-     const {title, button1, button1_target, button2, button2_target, button2Disabled=false} = props
+     const {title, button1, button1_target, button2, button2_target, button2Disabled=false, buttonAClick} = props
      const btnExportRef = useRef()
 
      useEffect(()=>{
@@ -14,7 +14,7 @@ const TableAction = (props)=>{
                <div className="col-12">
                     <h5 className="card-title mt-1 float-start">{title}</h5>
                     <div className="btn-group float-end">
-                         <MDBBtn outline data-bs-toggle="modal" data-bs-target={button1_target}>{button1}</MDBBtn>
+                         <MDBBtn outline data-bs-target={button1_target} onClick={buttonAClick}>{button1}</MDBBtn>
                          <MDBBtn color="success" outline  data-bs-toggle="modal" ref={btnExportRef} data-bs-target={button2_target}>{button2}</MDBBtn>
                     </div>
                </div>

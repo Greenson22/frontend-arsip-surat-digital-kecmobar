9fcdb0a890630b0d01c9445123fdd31dd78cc0ec
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:10:54 +0800

    refactor : menambahkan add multiple incomingmail modal

--- src/components/Layouts/IncomingMailEmptyLayout.jsx
+++ src/components/Layouts/IncomingMailEmptyLayout.jsx
@@ -1,18 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { Card , CardHeader, CardBody} from "../Elements/Card"
 import { Table, TableHead } from '../Elements/Table'
 import { TitleBar, TableAction, AddIncomingMailModal, TableFilter } from '../Fragments'
-import { AddMultipleIncomingmailModal } from '../Fragments/Modals'
+import { AddMultipleIncomingmailModal, AddIncomingmailModalV2 } from '../Fragments/Modals'
 
 const IncomingMailEmptyLayout = (props)=>{
      const columns = ["No", "Nomor agenda", "Nomor surat", "Tanggal surat", "Tanggal terima", "Asal surat", "Perihal", "Penerima", "Tindakan"]
-      
+     const [addModal, setAddModal] = useState(false)
+     const [addMultipleModal, setMultipleModal] = useState(false)
      return (
           <div>
                <TitleBar>Surat masuk</TitleBar>
                <Card>
                     <CardHeader>
-                         <TableAction title="Daftar surat masuk" button1="Tambah surat" button1_target="#add-mail-modal" button2="Ekspor" button2_target="#exportModal" button2Disabled={true}/>
+                         <TableAction title="Daftar surat masuk" button1="Tambah surat" button2="Ekspor" button2_target="#exportModal" button2Disabled={true} buttonAClick={()=>{setAddModal(true)}}/>
                     </CardHeader>
                     <CardBody>
                          <TableFilter />
@@ -21,8 +22,9 @@ const IncomingMailEmptyLayout = (props)=>{
                          </Table>
                     </CardBody>
                </Card>
-               <AddIncomingMailModal />
-               <AddMultipleIncomingmailModal />
+               {/* <AddIncomingMailModal /> */}
+               <AddIncomingmailModalV2 addModal={addModal} setAddModal={setAddModal} setMultipleModal={setMultipleModal} />
+               <AddMultipleIncomingmailModal addMultipleModal={addMultipleModal} setMultipleModal={setMultipleModal} />
           </div>
      )
 }

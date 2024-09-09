b85c4c90bb92f8bdf8623657c1715df664b2860e
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 21:25:34 +0800

    refactor : menambahkan setAddModal untuk mematikan modal

--- src/hooks/handle/add_incomingmail_modal/useHandleSubmit.jsx
+++ src/hooks/handle/add_incomingmail_modal/useHandleSubmit.jsx
@@ -1,11 +1,12 @@
 import { setCommand } from "../../../redux/slices/commandSlice"
 
-const useHandleSubmit = (event, dispatch)=>{
+const useHandleSubmit = (event, setAddModal, dispatch)=>{
      event.preventDefault()
      dispatch(setCommand({
           'command': 'post',
           'form_id' : 'add-incomingmail-form'
      }))
+     setAddModal(false)
 }
 
 export default useHandleSubmit

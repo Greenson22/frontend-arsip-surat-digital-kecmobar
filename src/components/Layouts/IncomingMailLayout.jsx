e3a27f5bb12c737b058835bfda7f09d6a644c82e
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-09 17:08:44 +0800

    refactor : merubah id modal menjadi lebih bagus

--- src/components/Layouts/IncomingMailLayout.jsx
+++ src/components/Layouts/IncomingMailLayout.jsx
@@ -20,7 +20,7 @@ const IncomingMailLayout = (props)=>{
                <TitleBar>Surat masuk</TitleBar>
                <Card>
                     <CardHeader>
-                         <TableAction title="Daftar surat masuk" button1="Tambah surat" button1_target="#addMailModal" button2="Ekspor" button2_target="#exportModal" />
+                         <TableAction title="Daftar surat masuk" button1="Tambah surat" button1_target="#add-mail-modal" button2="Ekspor" button2_target="#exportModal" />
                     </CardHeader>
                     <CardBody>
                          <TableFilter api={api} />

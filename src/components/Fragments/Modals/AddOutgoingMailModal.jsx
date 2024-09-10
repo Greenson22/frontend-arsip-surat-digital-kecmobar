ea62d31522a19e327c711703fea8e5fa4771e2f1
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-10 19:23:38 +0800

    refactor : merubah ke bentuk modal v2

--- src/components/Fragments/Modals/AddOutgoingMailModal.jsx
+++ src/components/Fragments/Modals/AddOutgoingMailModal.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
-
-import {Modal, ModalHeader, ModalBody, ModalFooter} from "../../Elements/Modal"
+import React, { useRef, useState } from 'react';
+import { MDBInput, MDBBtn, MDBModalBody, MDBModalFooter, MDBInputGroup } from 'mdb-react-ui-kit';
 
 import AnalisisIndicator from "../AnalisisIndicator"
 import { useDispatch } from 'react-redux';
 import { setCommand } from '../../../redux/slices/commandSlice';
 
-const AddOutgoingMailModal = ()=>{
+import { ModalV2, ModalHeader } from '../../Elements/ModalV2';
+import { useHandleInputFileChange } from '../../../hooks/handle/add_incomingmail_modal';
+import { useHandleBtnAnalysis } from '../../../hooks/handle/add_outgoingmail_modal';
+
+const AddOutgoingMailModal = (props)=>{
+     const { addModal, setAddModal } = props
      const dispatch = useDispatch()
+     const [analysis, setAnalysis] = useState(false)
+     const inputFileRef = useRef()
+     const formRef = useRef()
+     const previewRef = useRef()
 
      const handleSubmit = (event)=>{
           event.preventDefault()
@@ -17,36 +24,42 @@ const AddOutgoingMailModal = ()=>{
                'form_id' : 'add-outgoingmail-form'
           }))
      }
+     const handleBtnAnalisisClick = (event)=>{useHandleBtnAnalysis(event, inputFileRef, formRef, setAnalysis)}
+     const handleInputFileChange = (event)=>{useHandleInputFileChange(event, previewRef)}
 
      return(
-          <Modal id="add-outgoingmail-modal">
+          <ModalV2 open={addModal} onClose={(event)=>setAddModal(false)}>
                <ModalHeader title="Tambah surat keluar"/>
-               <ModalBody>
-                    <form action="" id='add-outgoingmail-form' onSubmit={handleSubmit}>
-                         <label htmlFor="" className="from-label">Unggah file surat</label>
-                         <br/><sub className="">*pdf, png, jpeg, jpg</sub>
-                         <input id="document" type="file" accept="application/pdf" name="" className="form-control mt-2" required/>
-                         <AnalisisIndicator/>
+               <MDBModalBody>
+                    <form action="" id='add-outgoingmail-form' onSubmit={handleSubmit} ref={formRef}>
+                         <label htmlFor="" className="from-label">Unggah file surat *.pdf</label>
+                         <MDBInputGroup>
+                              <MDBInput id='document' type='file' accept="application/pdf" required onChange={handleInputFileChange} ref={inputFileRef} ></MDBInput>
+                              <MDBBtn size='sm' onClick={handleBtnAnalisisClick} type='button'>Analisa</MDBBtn>
+                         </MDBInputGroup>
+
+                         <AnalisisIndicator isActive={analysis}/>
+                         <iframe width={'100%'} height={'0px'} ref={previewRef}></iframe>
                          <hr className="mb-4"/>
 
                          <div className="row">
                               <div className="col-6">
                                    <MDBInput id="agenda-number" label="Nomor agenda" type="text" className='mb-2'/>
-                                   <MDBInput id="letter-number" label="Nomor surat" type="text" className='mb-2' required/>
+                                   <MDBInput id="letter-number" label="Nomor surat" type="text" className='mb-2' required defaultValue={' '}/>
                               </div>
                               <div className="col-6">
                                    <MDBInput id="letter-date" label="Tanggal surat" type="date" className='mb-2' required/>
-                                   <MDBInput id="destination" label="Tujuan surat" type="text" className='mb-2' required/>
+                                   <MDBInput id="destination" label="Tujuan surat" type="text" className='mb-2' required defaultValue={' '}/>
                               </div>
                          </div>
-                         <MDBInput id="subject" label="Perihal" type="text" className='mb-2' required/>
+                         <MDBInput id="subject" label="Perihal" type="text" className='mb-2' required defaultValue={' '}/>
                     </form>
-               </ModalBody>
-               <ModalFooter>
-                    <MDBBtn size='sm' color='secondary' data-bs-dismiss="modal">Tutup</MDBBtn>
-                    <MDBBtn size='sm' color='primary' form='add-outgoingmail-form'>Tambah surat</MDBBtn>
-               </ModalFooter>
-          </Modal>
+                </MDBModalBody>
+                <MDBModalFooter>
+                     <MDBBtn size='sm' color='secondary' onClick={(event)=>setAddModal(false)}>Tutup</MDBBtn>
+                     <MDBBtn size='sm' color='primary' form='add-outgoingmail-form'>Tambah surat</MDBBtn>
+                </MDBModalFooter>
+          </ModalV2>
      )
 }
 

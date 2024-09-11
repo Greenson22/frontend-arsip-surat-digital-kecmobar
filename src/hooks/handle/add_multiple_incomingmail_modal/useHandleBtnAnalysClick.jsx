79fda0a59a88e24e1ac16d1d038d7bb75bab98c7
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-11 19:36:57 +0800

    refactor : jika fileList null tidak melakukan aksi

--- src/hooks/handle/add_multiple_incomingmail_modal/useHandleBtnAnalysClick.jsx
+++ src/hooks/handle/add_multiple_incomingmail_modal/useHandleBtnAnalysClick.jsx
@@ -1,32 +1,37 @@
 import { usePostData } from "../../request"
 
 const handleBtnAnalysClick = (event, fileNote, fileList, setFileNote, outgoingmail) => {
-     const duplicateFileNote = fileNote.slice()
-
+     if(!fileList)return
      fileList.map((file, index) => {
-          const formData = new FormData()
-          formData.append('file', fileList[index])
-          if(outgoingmail){
-               formData.append('model_type', 'outgoingmail')
+          const duplicateFileNote = fileNote.slice()
+          if(fileNote[index].status !== 'check'){
+               const formData = new FormData()
+               formData.append('file', fileList[index])
+               if(outgoingmail){
+                    formData.append('model_type', 'outgoingmail')
+               }
+               console.log('sedang menganalisa beberapa file')
+
+               duplicateFileNote[index].status = 'loading'
+               setFileNote(duplicateFileNote)
+
+               usePostData(import.meta.env.VITE_GENAI_API_KEY, formData, localStorage.getItem('accessToken'), (response) => {
+                    const duplicateFileNoteResponse = fileNote.slice()
+                    const data = response.data
+                    duplicateFileNoteResponse[index].entities = data.entities
+                    duplicateFileNoteResponse[index].status = 'check'
+                    setFileNote(duplicateFileNoteResponse)
+                    console.log('satu proses selesai')
+               }, (error)=>{
+                    const duplicateFileNoteResponse = fileNote.slice()
+                    duplicateFileNote[index].status = 'fail'
+                    setFileNote(duplicateFileNoteResponse)
+                    console.log('satu proses gagal')
+                    if (error.response.status===401){
+                         window.location.reload()
+                    }
+               })
           }
-          console.log('sedang menganalisa beberapa file')
-
-          duplicateFileNote[index].status = 'loading'
-          setFileNote(duplicateFileNote)
-
-          usePostData(import.meta.env.VITE_GENAI_API_KEY, formData, localStorage.getItem('accessToken'), (response) => {
-               const duplicateFileNoteResponse = fileNote.slice()
-               const data = response.data
-               duplicateFileNoteResponse[index].entities = data.entities
-               duplicateFileNoteResponse[index].status = 'check'
-               setFileNote(duplicateFileNoteResponse)
-               console.log('satu proses selesai')
-          }, (error)=>{
-               const duplicateFileNoteResponse = fileNote.slice()
-               duplicateFileNote[index].status = 'fail'
-               setFileNote(duplicateFileNoteResponse)
-               console.log('satu proses gagal')
-          })
      })
 }
 

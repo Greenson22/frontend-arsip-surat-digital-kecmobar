4ccc22eea24541308f7ea81f80b9abdaf61a504a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-12 19:26:17 +0800

    refactor : sedikit perubahan

--- src/hooks/pagination/usePaginationLocalStorage.jsx
+++ src/hooks/pagination/usePaginationLocalStorage.jsx
@@ -3,12 +3,14 @@ import useUrlParams from '../url/useUrlParams'
 const usePaginationLocalStorage = (url)=>{
      const urlParams = useUrlParams(url) // mengambil parameter dari url 
      let page = urlParams.get('page') // mengambil parameter page
+     let pageSize = urlParams.get('page_size') // mengambil parameter page size
      
      if (!page){page = 1} // jika page tidak ada maka beri nilai 1
+     if (!pageSize){pageSize = 5} // jika page size tidak ada maka beri nilai 1
      // data dimasukan kedalam dictionary
-     const dataDict = { 
+     const dataDict = {
           'page': page,
-          'page_size': urlParams.get('page_size'),
+          'page_size': pageSize,
      }
      // menetapkan nilai di dalam session storage dari dataDict
      sessionStorage.setItem('pagination', JSON.stringify(dataDict))

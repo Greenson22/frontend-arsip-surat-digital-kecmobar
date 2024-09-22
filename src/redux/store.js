f95c2220dda69994286bb96c0734dd580ebcd598
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-22 17:46:47 +0800

    refactor : menambahkan pagination reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore} from "@reduxjs/toolkit"
 import dataReducer from "./slices/dataSlice"
 import commandReducer from "./slices/commandSlice"
+import paginationReducer from "./slices/paginationSlice"
 
 const store = configureStore({
      reducer:{
           dataSlice: dataReducer,
           commandSlice: commandReducer,
+          paginationSlice: paginationReducer
      }
 })
 

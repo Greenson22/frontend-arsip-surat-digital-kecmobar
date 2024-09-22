ee02e1723a94a0eaeb8f474ddf5a64907fa22714
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-22 21:42:48 +0800

    refactor : menggunakan page dan pageSize dengan state management

--- src/components/Fragments/TableBody/UserManagementTableBody.jsx
+++ src/components/Fragments/TableBody/UserManagementTableBody.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { TableBody, RowAction } from "../../Elements/Table"
 import { useFormattedDate } from "../../../hooks"
+import { useSelector } from "react-redux"
 
 const UserManagementTableBody = (props)=>{
      const {data} = props
-     const pagination = JSON.parse(sessionStorage.getItem('pagination'))
+     const page = useSelector(state=>state.paginationSlice.page)
+     const pageSize = useSelector(state=>state.paginationSlice.pageSize)
      
      return (
           <TableBody>
                {data.map((user, index)=>{
                          return(
                               <tr key={user.id}>
-                                   <td>{index+1+((pagination['page']-1)*pagination['page_size'])}</td>
+                                   <td>{index+1+((page-1)*pageSize)}</td>
                                    <td>{user.first_name+' '+user.last_name}</td>
                                    <td>{user.username}</td>
                                    <td>{user.is_superuser ? 'admin' : 'user'}</td>

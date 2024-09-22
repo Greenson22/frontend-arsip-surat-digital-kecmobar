2a4ff1981a3bb27e42816e5adf21ca287579d131
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-22 17:40:21 +0800

    add : slice pagination untuk menangani page dan pageSize

---
+++ src/redux/slices/paginationSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const paginationSlice = createSlice({
+     name: 'data',
+     initialState: {
+          page: 1,
+          pageSize: 5,
+     },
+     reducers: {
+          setPage: (state, action)=> {
+               state.page = action.payload
+          },
+          setPageSize: (state, action)=> {
+               state.pageSize = action.payload
+          }
+     }
+})
+
+export const { setPage, setPageSize } = paginationSlice.actions
+export default paginationSlice.reducer

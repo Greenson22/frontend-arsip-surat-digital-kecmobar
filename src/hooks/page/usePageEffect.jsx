5ff0a0f6769c42d72fe37ae2b6ff939900e76adc
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-22 21:43:10 +0800

    refactor : menambahkan page dan pageSize

--- src/hooks/page/usePageEffect.jsx
+++ src/hooks/page/usePageEffect.jsx
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const usePageEffect = (callBackAction)=>{
      const command = useSelector(state=>state.commandSlice.command)
+     const page = useSelector(state=>state.paginationSlice.page)
+     const pageSize = useSelector(state=>state.paginationSlice.pageSize)
      const dispatch = useDispatch()
      
      useEffect(()=>{
-          callBackAction(command, dispatch)
-     }, [command])
+          callBackAction(command, page, pageSize, dispatch)
+     }, [command, page, pageSize])
      
      useEffect(()=>{
           localStorage.setItem('search_value', '')

0e96eeb3786510ef6a5203cef22195bdc804e353
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-22 21:43:59 +0800

    refactor : menyimpan page dan pageSize di state

--- src/hooks/pagination/usePaginationLocalStorage.jsx
+++ src/hooks/pagination/usePaginationLocalStorage.jsx
@@ -1,19 +1,16 @@
 import useUrlParams from '../url/useUrlParams'
+import { setPage, setPageSize } from '../../redux/slices/paginationSlice'
 
-const usePaginationLocalStorage = (url)=>{
+const usePaginationLocalStorage = (url, dispatch)=>{
      const urlParams = useUrlParams(url) // mengambil parameter dari url 
      let page = urlParams.get('page') // mengambil parameter page
      let pageSize = urlParams.get('page_size') // mengambil parameter page size
      
      if (!page){page = 1} // jika page tidak ada maka beri nilai 1
      if (!pageSize){pageSize = 5} // jika page size tidak ada maka beri nilai 1
-     // data dimasukan kedalam dictionary
-     const dataDict = {
-          'page': page,
-          'page_size': pageSize,
-     }
-     // menetapkan nilai di dalam session storage dari dataDict
-     sessionStorage.setItem('pagination', JSON.stringify(dataDict))
+
+     dispatch(setPage(page))
+     dispatch(setPageSize(pageSize))
 }
 
 export default usePaginationLocalStorage

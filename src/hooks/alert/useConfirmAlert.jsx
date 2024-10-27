0f31ca9c704e242a2e4fd20cffdd594da1d7e186
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-10-27 15:33:41 +0800

    refactor : menambahkan parameter ke dalam confirm alert

--- src/hooks/alert/useConfirmAlert.jsx
+++ src/hooks/alert/useConfirmAlert.jsx
@@ -1,15 +1,18 @@
 import Swal from 'sweetalert2'
 
-const useConfirmAlert = (call_back_result)=>{
+const defaultTitle = "Apakah kamu ingin menghapus data ini??"
+const defaultText = "Jika anda setuju, maka data tidak bisa dikembalikan!"
+
+const useConfirmAlert = (call_back_result, title=defaultTitle, text=defaultText, confirmButtonText="Hapus", cancelButtonText="Batal")=>{
      Swal.fire({
-          title: "Apakah kamu ingin menghapus data ini??",
-          text: "Jika anda setuju, maka data tidak bisa dikembalikan!",
+          title: title,
+          text: text,
           icon: "warning",
           showCancelButton: true,
           confirmButtonColor: "#ffc107",
           cancelButtonColor: "#3085d6",
-          confirmButtonText: "Hapus",
-          cancelButtonText: "Batal"
+          confirmButtonText: confirmButtonText,
+          cancelButtonText: cancelButtonText
         }).then(call_back_result);
 }
 

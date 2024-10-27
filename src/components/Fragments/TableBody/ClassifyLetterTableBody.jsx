da72b69c38ccf47cd92cb246ad28b15e6fb4622a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-10-27 15:32:57 +0800

    add : table body untuk halaman classify

---
+++ src/components/Fragments/TableBody/ClassifyLetterTableBody.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { MDBBtn } from 'mdb-react-ui-kit'
+import { TableBody, RowAction } from '../../Elements/Table'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCommand } from '../../../redux/slices/commandSlice'
+
+const ClassifyLetterTableBody = (props)=>{
+     const {data} = props
+     const page = useSelector(state=>state.paginationSlice.page)
+     const pageSize = useSelector(state=>state.paginationSlice.pageSize)
+     const dispatch = useDispatch()
+
+     return(
+          <TableBody>
+               {data.map((surat, index)=>{
+                         return(
+                              <tr key={surat.id}>
+                                   <td>{index+1+((page-1)*pageSize)}</td>
+                                   <td >{surat.agenda_number}</td>
+                                   <td >{surat.letter_number}</td>
+                                   <td >{surat.letter_date}</td>
+                                   <td >{surat.received_date}</td>
+                                   <td >{surat.source}</td>
+                                   <td >{surat.subject}</td>
+                                   <td >{surat.recipient}</td>
+                                   <td >{surat.classify}</td>
+                                   <td>
+                                        <MDBBtn size='sm' color='warning' outline onClick={()=>{
+                                             dispatch(setCommand({
+                                                  'id':surat.id,
+                                                  'command':'classify'
+                                             }))
+                                        }}>Klasifikasi</MDBBtn>
+                                   </td>
+                              </tr>
+                         )
+                    })}               
+          </TableBody>
+     )
+} 
+
+export default ClassifyLetterTableBody

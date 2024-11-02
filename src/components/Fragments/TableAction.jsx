7e0ec3915a3040b01d4fbd25acd01ceac90b6346
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-11-02 12:39:08 +0800

    refactor : membuat TableAction menjadi lebih sederhana

--- src/components/Fragments/TableAction.jsx
+++ src/components/Fragments/TableAction.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useRef } from "react"
 import { MDBBtn } from "mdb-react-ui-kit"
 
 const TableAction = (props)=>{
-     const {title, buttonChildren, buttonClick} = props
+     const {title, buttonPrimary=null, buttonSecondary=null} = props
 
      return(
           <div className="row">
                <div className="col-12">
                     <h5 className="card-title mt-1 float-start">{title}</h5>
                     <div className="btn-group float-end">
-                         <MDBBtn outline onClick={buttonClick}>{buttonChildren}</MDBBtn>
+                         {buttonSecondary && 
+                              <a href="/classify"><MDBBtn outline onClick={buttonSecondary.click}>{buttonSecondary.children}</MDBBtn></a>}
+                         {buttonPrimary && 
+                              <MDBBtn outline onClick={buttonPrimary.click}>{buttonPrimary.children}</MDBBtn>}
                     </div>
                </div>
           </div>

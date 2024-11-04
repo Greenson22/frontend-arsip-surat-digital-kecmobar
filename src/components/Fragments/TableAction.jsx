6a91c18604f9b951780caff6ce7bdb1496532304
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-11-04 19:28:56 +0800

    refactor : tombol-tombol yang bisa dikostum dari dictionary

--- src/components/Fragments/TableAction.jsx
+++ src/components/Fragments/TableAction.jsx
@@ -9,8 +9,12 @@ const TableAction = (props)=>{
                <div className="col-12">
                     <h5 className="card-title mt-1 float-start">{title}</h5>
                     <div className="btn-group float-end">
-                         {buttonSecondary && 
-                              <a href="/classify"><MDBBtn outline onClick={buttonSecondary.click}>{buttonSecondary.children}</MDBBtn></a>}
+                         {/* tombol paling kiri */}
+                         {buttonSecondary && buttonSecondary.href === null &&
+                              <MDBBtn outline onClick={buttonSecondary.click}>{buttonSecondary.children}</MDBBtn>}
+                         {buttonSecondary && buttonSecondary.href &&
+                              <a href={buttonSecondary.href}><MDBBtn outline onClick={buttonSecondary.click}>{buttonSecondary.children}</MDBBtn></a>}
+                         {/* tombol paling kanan */}
                          {buttonPrimary && 
                               <MDBBtn outline onClick={buttonPrimary.click}>{buttonPrimary.children}</MDBBtn>}
                     </div>

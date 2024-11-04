5bc560621f870ad4c009e75bd3c75f2017b27672
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-11-04 19:29:56 +0800

    refactor : perubahan bentuk tombol di table action

--- src/components/Layouts/ClassifyLetterLayout.jsx
+++ src/components/Layouts/ClassifyLetterLayout.jsx
@@ -3,25 +3,24 @@ import { Card , CardHeader, CardBody} from "../Elements/Card"
 import { Table, TableHead } from "../Elements/Table"
 import { MDBBtn } from 'mdb-react-ui-kit'
 import { useDispatch, useSelector } from 'react-redux'
-import { TitleBar, TableFilter, TableAction, IncomingMailTableBody } from '../Fragments'
+import { TitleBar, TableFilter, TableAction } from '../Fragments'
 import { useSetPage } from "../../hooks"
 import ClassifyLetterTableBody from '../Fragments/TableBody/ClassifyLetterTableBody'
 
 const ClassifyLetterLayout = (props)=>{
      const columns = ["No", "Nomor agenda", "Nomor surat", "Tanggal surat", "Tanggal terima", "Asal surat", "Perihal", "Penerima", "Kategori","Tindakan"]
      const {data} = props
-     const iData = useSelector(state=>state.dataSlice.iData)
      const api = import.meta.env.VITE_INCOMINGMAIL_API_KEY
      const dispatch = useDispatch()
-     const [addModal, setAddModal] = useState(false)
-     const [addMultipleModal, setMultipleModal] = useState(false)
+     const buttonPrimary = {children:'Klasifikasi Semua', click: ()=>{}}
+     const backButton = {children:'Kembali', click: ()=>{}, href:'/incoming_mail'}
      
      return (
           <div>
                <TitleBar>Klasifikasi Surat masuk</TitleBar>
                <Card>
                     <CardHeader>
-                         <MDBBtn className='float-end'>Klasifikasi Semua</MDBBtn>
+                         <TableAction title="Daftar Surat Masuk" buttonPrimary={buttonPrimary} buttonSecondary={backButton}></TableAction>
                     </CardHeader>
                     <CardBody>
                          <TableFilter api={api} />

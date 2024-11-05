e77879bc6170cb25f504cce12b0e887c335bf1ae
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-11-05 16:15:06 +0800

    add : saat tombol klasifikasi ditekan maka akan melakukan klasifikasi terhadap baris data yang diinginkan

---
+++ src/hooks/handle/classify_letter_page/useHandleConfirmed.jsx
@@ -0,0 +1,40 @@
+import { setCommand } from '../../../redux/slices/commandSlice'
+import { usePostData } from '../../../hooks'
+
+const useHandleConfirmed = (note, notes, setNotes, dispatch, setTableRow)=>{
+     const clasify_url = import.meta.env.VITE_CLASSIFY_LETTER_API_KEY
+     const token = localStorage.getItem('accessToken')
+
+     // mengubah status note
+     setNoteStatus('loading', note, notes, setNotes)
+     // mengirim permintaan ke api untuk klasifikasi surat
+     usePostData(clasify_url, {'id':note.id}, token, response=>{
+          setNoteStatus(null, note, notes, setNotes)
+          console.log(response)
+          // mengirim permintaan ke action untuk melakukan reset terhadap data
+          dispatch(setCommand({
+               'command':'classify'
+          }))
+          // merubah class dari row di table
+          setTableRow(note, 'success')
+     }, error=>{
+          setNoteStatus(null, note, notes, setNotes)
+          console.log(error)
+          // merubah class dari row di table
+          setTableRow(note, 'fail')
+     })
+}
+
+// mengubah status note
+const setNoteStatus = (status, note, notes, setNotes)=>{
+     const tempNote = notes.slice()
+     tempNote.map(item=>{
+          if (item.id === note.id){
+               item.status = status
+          }
+     })
+     console.log(tempNote)
+     setNotes(tempNote)
+}
+
+export default useHandleConfirmed
